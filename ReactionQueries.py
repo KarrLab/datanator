cb8945074ba82654aa5f2ceb1b1bd659e033c714
Yosef <yosefdroth@gmail.com>
2017-02-03 08:11:13 -0500

    queries

--- ReactionQueries.py
+++ ReactionQueries.py
@@ -1,29 +1,45 @@
 import openpyxl
-from InchiGenerator import generateGenericInchi
-from InchiGenerator import getSabioNameToInchiDict
+import InchiGenerator
+import QueryStringManipulator
 
 
 class Compound:
-	def __init__(self, ID, inchiSmiles, sabioNames):
-		self.ID = ID
+	def __init__(self, id, inchiSmiles = "", sabioNames = []):
+		self.id = id
 		self.inchiSmiles = inchiSmiles
 		self.sabioNames = sabioNames
 
 
+
 class ReactionQuery:
-	def __init__(self, ID):
-		self.ID = ID
-		self.substrates = []
-		self.products = []
-		self.numParticipants = ""
+	def __init__(self, id):
+		self.id = id
+		self.reactionString = ""
+		self.substrates = [] #this is a list of compound objects
+		self.products = [] #this is a list of compound objects
+		self.numParticipants = [] #this is an array of two numbers, first number for substrates, second for products
 		self.keggID = ""
 		self.ECNumber = ""
 
-	def generateQueryString():
-		return ""
+	def getQueryString(self):
+		subAndProd = []
+		subNames = []
+		prodNames = []
+		for compound in self.substrates:
+			if len(compound.sabioNames)>0:
+				subNames.append(compound.sabioNames)
+		for compound in self.products:
+			if len(compound.sabioNames)>0:
+				subNames.append(compound.sabioNames)
+		subAndProd.append(subNames)
+		subAndProd.append(prodNames)
+		searchString = QueryStringManipulator.getQuerySearchString(subAndProd)
+		return searchString
 
 	def generateLiftedReactionQuery():
 		return ""
+
+
 
 class LiftedReactionQuery:
 	def __init__(self, ID):
@@ -33,32 +49,30 @@ class LiftedReactionQuery:
 		self.numParticipants = ""
 		self.genericECNumber = ""
 	
-	def generateQueryString():
+	def getQueryString():
 		return ""
 
 
-
-
-#make a method that takes in the excel sheet, and outputs a list of all the reactions.
-#internally, this method should have an ID to compound Dict, but it shouldnt share it. 
-
-#takes in an excel sheet. ouputs reation objects
+#Input an excel sheet
+#It outputs a list of reactionQuery Objects
 def generateReactionQueries(filename):
+	reactionQueries = [] #this is the only output of this method. Its a list of reactionQueries
 	idToCompound = {} #this will be a dict used to find compound information about each metab id
+	
 	wb = openpyxl.load_workbook(filename=filename)
 	ws = wb.get_sheet_by_name('Metabolites')
 
 	#this gets a dict that is used to correlate inchiString with Sabio's name for that compount
-	sabioNameToInchiDict = getSabioNameToInchiDict()
+	sabioNameToInchiDict = InchiGenerator.getSabioNameToInchiDict()
 
 	#this instantiates a compound object for each metabolite in the excel sheet
 	i = 0
-	while i < len (ws.columns[0]):
+	while i < len(ws.columns[0]):
 		id = ws.columns[0][i].value
 		smilesOrInchi = ws.columns[1][i].value
 		sabioNames = []
 		if smilesOrInchi != None:
-			genericInchi = generateGenericInchi(smilesOrInchi)
+			genericInchi = InchiGenerator.generateGenericInchi(smilesOrInchi)
 			for name in sabioNameToInchiDict:
 				if sabioNameToInchiDict[name] == genericInchi:
 					sabioNames.append(name)
@@ -67,20 +81,58 @@ def generateReactionQueries(filename):
 		idToCompound[id] = comp
 		i += 1
 
-	for id in idToCompound:
-		print idToCompound[id].__dict__
 
-	
-
-
+	#this creates a dictionary of reaction ID to reaction string
+	#to work on: making it possible to only enter one row
+	#to work on: make it possible not to have any reaction string data to begin with
 	ws = wb.get_sheet_by_name('Reactions')
-	reactionArray = []
-	for thing in ws.columns[0]:
-		reactionArray.append(thing.value)
-	print reactionArray
+	idToReactionDict = {}
+	i = 0
+	while i < len(ws.columns[0]):
+		id = "{}".format(ws.columns[0][i].value)
+		if len("{}".format(ws.columns[1][i].value))>0:
+			idToReactionDict[id] = "{}".format(ws.columns[1][i].value)
+		else:
+			idToReactionDict[id] = id
+		i += 1
 
 
+	#this is is where the ReactionQuery objects are instantiated
+	#each reaction is parsed, each metabolite ID is converted to a Compound object
+	#then the fields for each ReactionQuery object are set
+	#finally the ReactionQuery object is added to the reactionQueries list
+	for id in idToReactionDict:
+		query = ReactionQuery(id)
+		query.reactionString = idToReactionDict[id]
+
+		balancedMetab = QueryStringManipulator.getParsedReaction(idToReactionDict[id])
+		query.numParticipants = [len(balancedMetab[0]), len(balancedMetab[1])]
+
+		compoundBalancedMetab = []
+		for side in balancedMetab:
+			compoundSide = []
+			for metab in side:
+				try:
+					compoundSide.append(idToCompound[metab])
+				except:
+					newUnrecognizedCompound = Compound(metab)
+					compoundSide.append(newUnrecognizedCompound)
+			compoundBalancedMetab.append(compoundSide)
+		query.substrates = compoundBalancedMetab[0]
+		query.products = compoundBalancedMetab[1]
+		reactionQueries.append(query)
+
+	return reactionQueries
+
+
+
+
+
+		
 
 if __name__ == '__main__':
-	filename='SmilesStuff2.xlsx'
-	generateReactionQueries(filename)
+	filename='SmilesStuff.xlsx'
+	reactionQueries = generateReactionQueries(filename)
+	for reaction in reactionQueries:
+		print reaction.__dict__
+		print reaction.getQueryString()
