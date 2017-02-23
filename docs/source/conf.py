8cb3c0a30f6646a561b25f8ef5bb6b8df10e3b22
Yosef <yosefdroth@gmail.com>
2017-02-23 00:51:29 -0500

    adding docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,13 +32,10 @@ import sphinx_rtd_theme
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-
 extensions = ['sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode']
-
-#extensions = ['sphinxcontrib.exceltable']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
