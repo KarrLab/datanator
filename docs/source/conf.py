aff98d1bea4feda829715bc31edda5dd55bc382a
Yosef <yosefdroth@gmail.com>
2017-02-24 08:03:25 -0500

    adding docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,21 @@
+Skip to content
+This repository
+Search
+Pull requests
+Issues
+Gist
+ @yosefdroth
+ Sign out
+ Watch 7
+  Star 0
+ Fork 0 KarrLab/Kinetic-Datanator
+ Code  Issues 0  Pull requests 0  Projects 0  Wiki  Pulse  Graphs  Settings
+Tree: 86608717fc Find file Copy pathKinetic-Datanator/docs/source/conf.py
+8660871  an hour ago
+@yosefdroth yosefdroth adding docs
+1 contributor
+RawBlameHistory     
+189 lines (142 sloc)  5.5 KB
 # -*- coding: utf-8 -*-
 #
 # KineticDatanator documentation build configuration file, created by
@@ -15,18 +33,19 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+
 #
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-#import sphinx_rtd_theme
+import sphinx_rtd_theme
 
 
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-#needs_sphinx = '1.3.6'
+# needs_sphinx = '1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -36,10 +55,8 @@ extensions = ['sphinx.ext.intersphinx',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode']
 
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -186,3 +203,5 @@ epub_exclude_files = ['search.html']
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status Help
