ab9ffa830123ba84e5534e6023011572d46f66f5
Yosef <yosefdroth@gmail.com>
2017-02-24 07:13:49 -0500

    adding docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,6 @@ extensions = ['sphinx.ext.intersphinx',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode']
 
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
