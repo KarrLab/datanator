726b0f59356dc57d2c06dde716160bbe367b7dc4
Jonathan Karr <jonrkarr@gmail.com>
2017-05-04 01:03:23 -0400

    cleaning up

--- kinetic_datanator/data_structs.py
+++
@@ -1,6 +0,0 @@
-class Compound(object):
-
-    def __init__(self, id, inchi_smiles="", sabioNames=[]):
-        self.id = id
-        self.inchi_smiles = inchi_smiles
-        self.sabioNames = sabioNames
