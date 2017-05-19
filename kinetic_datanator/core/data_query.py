d99c3af5b886bc40a995ea93e7ae65e544528b2d
Jonathan Karr <jonrkarr@gmail.com>
2017-05-18 21:06:53 -0400

    working on kinetic searching

---
+++ kinetic_datanator/core/data_query.py
@@ -0,0 +1,205 @@
+"""
+:Author: Jonathan Karr <jonrkarr@gmail.com>
+:Author: Yosef Roth <yosefdroth@gmail.com>
+:Date: 2017-05-16
+:Copyright: 2017, Karr Lab
+:License: MIT
+"""
+
+from kinetic_datanator.core import filter
+from kinetic_datanator.core import data_model
+import abc
+import kinetic_datanator.core.data_source
+import math
+import numpy
+import pint
+import six
+
+unit_registry = pint.UnitRegistry()
+
+
+class DataQuery(six.with_metaclass(abc.ABCMeta, object)):
+    """ Represents a query of a data source
+
+    1. Find observations for the exact or similar model components
+
+    2. Filter out observations from disimilar genetic and environmental conditions and 
+      rank the remaing observations by their similarity to the desired genetic and environmental 
+      conditions
+
+      * Taxonomy
+      * Genetic variation (wildtype/mutant)
+      * Temperature
+      * pH
+
+    3. Calculate a statistical representation of the relevant observations
+
+    Attributes:
+        filters (:obj:`list` of :obj:`filter.Filter`): list of filters        
+    """
+
+    def __init__(self,
+                 taxon=None, max_taxon_dist=None, taxon_dist_scale=None, include_variants=False,
+                 temperature=37., temperature_std=1.,
+                 ph=7.5, ph_std=0.3):
+        """
+        Args:
+            taxon (:obj:`str`, optional): target taxon
+            max_taxon_dist (:obj:`int`, optional): maximum taxonomic distance to include
+            taxon_dist_scale (:obj:`float`, optional): The scale of the taxonomic distance scoring distribution. 
+                This determines how quickly the score falls to zero away from zero.
+            include_variants (:obj:`bool`, optional): if :obj:`True`, also include observations from mutant taxa
+            temperature (:obj:`float`, optional): desired temperature to search for
+            temperature_std (:obj:`float`, optional): how much to penalize observations from other temperatures
+            ph (:obj:`float`, optional): desired pH to search for
+            ph_std (:obj:`float`, optional): how much to penalize observations from other pHs
+        """
+        # todo: filter media
+
+        self.filters = filters = []
+
+        if taxon:
+            filters.append(filter.TaxonomicDistanceFilter(taxon, max=max_taxon_dist, scale=taxon_dist_scale))
+
+        if not include_variants:
+            filters.append(filter.WildtypeFilter())
+
+        if not math.isnan(temperature) and not math.isnan(temperature_std):
+            filters.append(filter.TemperatureNormalFilter(temperature, temperature_std))
+
+        if not math.isnan(ph) and not math.isnan(ph_std):
+            filters.append(filter.PhNormalFilter(ph, ph_std))
+
+    def run(self, component):
+        """ 
+
+        1. Find observations for the exact or similar model components and genetic and environmental conditions
+        2. Rank the results by their similarity to the model component and the genetic and environmental conditions
+        3. Calculate a consensus statistical representation of the relevant observations
+
+        Args:
+            component (:obj:`str`): model component to find data for
+
+        Returns:
+            :obj:`list` of :obj:`data_model.Consensus`: statistical consensus of the relevant observations of 
+                :obj:`component` and the observations it was based on
+        """
+        obs = self.get_observed_values(component)
+        filter_result = self.filter_observed_values(component, obs)
+        return self.get_consensus(component, filter_result)
+
+    @abc.abstractmethod
+    def get_observed_values(self, component):
+        """ Find the observations relevant to :obj:`component`
+
+        Args:
+            component (:obj:`str`): model component to find data for
+
+        Returns:
+            :obj:`list` of :obj:`data_model.ObservedValue`: list of relevant observations
+        """
+        pass
+
+    def filter_observed_values(self, component, observations):
+        """ Filter out observations from dissimilar genetic and environmental conditions and
+        order the remaining observations by their similarity to specified genetic and 
+        environmental conditions.
+
+        Args:
+            component (:obj:`str`): model component to find data for
+            observations (:obj:`list` of :obj:`data_model.ObservedValue`): list of observations
+
+        Returns:
+            :obj:`filter.FilterResult`: filter result
+        """
+        return filter.FilterRunner(self.filters) \
+            .run(observations, return_info=True)
+
+    def get_consensus(self, component, filter_result):
+        """ Calculate a consensus statistical representation of the one or more observations
+
+        Args:
+            component (:obj:`str`): model component to find data for
+            filter_result (:obj:`filter.FilterResult`): filter result
+
+        Returns:
+            :obj:`list` of :obj:`data_model.Consensus`: statistical consensus of the relevant observations of 
+                :obj:`component` and the observations it was based on
+        """
+        # group observations by their subcomponents, attributes
+        grouped_obs = {}
+        for obs, score in zip(filter_result.observations, filter_result.scores):
+            if obs.attribute not in grouped_obs:
+                grouped_obs[obs.attribute] = {}
+            if obs.subcomponent:
+                if obs.subcomponent not in grouped_obs[obs.attribute]:
+                    grouped_obs[obs.attribute][obs.subcomponent] = []
+                grouped_obs[obs.attribute][obs.subcomponent].append((obs, score))
+            else:
+                if '__default__' not in grouped_obs[obs.attribute]:
+                    grouped_obs[obs.attribute]['__default__'] = []
+                grouped_obs[obs.attribute]['__default__'].append((obs, score))
+
+        # calculate weighted mean
+        consensus = []
+        for attribute, attribute_obs in grouped_obs.items():
+            for subcomponent, subcomponent_obs in attribute_obs.items():
+                obs = []
+                scores = []
+                values = []
+                errors = []
+                for ob, score in subcomponent_obs:
+                    obs.append(data_model.Evidence(observation=ob, relevance=score))
+                    scores.append(score)
+
+                    value = ob.value * unit_registry(ob.units)
+                    error = ob.error * unit_registry(ob.units)
+                    values.append(value.to_base_units().magnitude)
+                    errors.append(error.to_base_units().magnitude)
+
+                value = numpy.average(values, weights=scores)
+                error = numpy.sqrt(numpy.average(numpy.power(errors, 2), weights=scores))
+
+                consensus.append(data_model.Consensus(
+                    component=component,
+                    attribute=attribute,
+                    value=value,
+                    error=error,
+                    units=units,
+                    method=data_model.ConsensusMethod.weighted_mean,
+                    evidence=obs))
+
+        return consensus
+
+
+class CachedDataSourceQuery(DataQuery):
+    """ Represents a query of a cached data source
+
+    Attributes:
+        data_source (:obj:`kinetic_datanator.core.data_source.CachedDataSource`): cached data source
+    """
+
+    def __init__(self,
+                 taxon=None, max_taxon_dist=None, taxon_dist_scale=None, include_variants=False,
+                 temperature=37., temperature_std=1.,
+                 ph=7.5, ph_std=0.3,
+                 data_source=None):
+        """
+        Args:            
+            taxon (:obj:`str`, optional): target taxon
+            max_taxon_dist (:obj:`int`, optional): maximum taxonomic distance to include
+            taxon_dist_scale (:obj:`float`, optional): The scale of the taxonomic distance scoring distribution. 
+                This determines how quickly the score falls to zero away from zero.
+            include_variants (:obj:`bool`, optional): if :obj:`True`, also include observations from mutant taxa
+            temperature (:obj:`float`, optional): desired temperature to search for
+            temperature_std (:obj:`float`, optional): how much to penalize observations from other temperatures
+            ph (:obj:`float`, optional): desired pH to search for
+            ph_std (:obj:`float`, optional): how much to penalize observations from other pHs
+            data_source (:obj:`kinetic_datanator.core.data_source.CachedDataSource`, optional): cached data source
+        """
+        super(CachedDataSourceQuery, self).__init__(
+            taxon=taxon, max_taxon_dist=max_taxon_dist, taxon_dist_scale=taxon_dist_scale, include_variants=include_variants,
+            temperature=temperature, temperature_std=temperature_std,
+            ph=ph, ph_std=ph_std)
+
+        self.data_source = data_source
--- tests/data_query/test_reaction_kinetics.py
+++ tests/data_query/test_reaction_kinetics.py
@@ -8,29 +8,292 @@
 :License: MIT
 """
 
+from kinetic_datanator.core import data_model
+from kinetic_datanator.data_source import sabio_rk
+from kinetic_datanator.data_query import reaction_kinetics
+from kinetic_datanator.util import molecule_util
 import unittest
 
 
-class TestSabioRkUtil(unittest.TestCase):
+class TestReactionKineticsQuery(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        cls.sabio_rk = sabio_rk.SabioRkUtil()
+    def setUp(self):
+        self.reaction_1_1_1_55 = data_model.Reaction(
+            participants=[
+                data_model.ReactionParticipant(
+                    specie=data_model.Specie(
+                        id='Lactaldehyde',
+                        structure='InChI=1S/C3H6O2/c1-3(5)2-4/h2-3,5H,1H3'),
+                    coefficient=-1),
+                data_model.ReactionParticipant(
+                    specie=data_model.Specie(
+                        id='NADPH',
+                        structure=(
+                            'InChI=1S/C21H30N7O17P3'
+                            '/c22-17-12-19(25-7-24-17)28(8-26-12)21-16(44-46(33,34)35)14(30)11(43-21)6-41-48(38,39)45-47(36,37)'
+                            '40-5-10-13(29)15(31)20(42-10)27-3-1-2-9(4-27)18(23)32'
+                            '/h1,3-4,7-8,10-11,13-16,20-21,29-31H,2,5-6H2,(H2,23,32)(H,36,37)(H,38,39)(H2,22,24,25)(H2,33,34,35)'
+                            '/t10-,11-,13-,14-,15-,16-,20-,21-/m1/s1')),
+                    coefficient=-1),
+                data_model.ReactionParticipant(
+                    specie=data_model.Specie(
+                        id='H+',
+                        structure='InChI=1S/p+1'),
+                    coefficient=-1),
+                data_model.ReactionParticipant(
+                    specie=data_model.Specie(
+                        id='1,2-Propanediol',
+                        structure='InChI=1S/C3H8O2/c1-3(5)2-4/h3-5H,2H2,1H3'),
+                    coefficient=1),
+                data_model.ReactionParticipant(
+                    specie=data_model.Specie(
+                        id='NADP+',
+                        structure=(
+                            'InChI=1S/C21H28N7O17P3'
+                            '/c22-17-12-19(25-7-24-17)28(8-26-12)21-16(44-46(33,34)35)14(30)11(43-21)6-41-48(38,39)'
+                            '45-47(36,37)40-5-10-13(29)15(31)20(42-10)27-3-1-2-9(4-27)18(23)32'
+                            '/h1-4,7-8,10-11,13-16,20-21,29-31H,5-6H2,(H7-,22,23,24,25,32,33,34,35,36,37,38,39)'
+                            '/t10-,11-,13-,14-,15-,16-,20-,21-/m1/s1')),
+                    coefficient=1),
+            ])
+
+        self.reaction_not_1_1_1_52 = data_model.Reaction(
+            participants=[
+                data_model.ReactionParticipant(
+                    specie=data_model.Specie(
+                        id='Lactaldehyde',
+                        structure='InChI=1S/C3H6O2xxxxxx/c1-3(5)2-4/h2-3,5H,1H3'),
+                    coefficient=-1),
+                data_model.ReactionParticipant(
+                    specie=data_model.Specie(
+                        id='NADPH',
+                        structure=(
+                            'InChI=1S/C21H30N7O17P3'
+                            '/c22-17-12-19(25-7-24-17)28(8-26-12)21-16(44-46(33,34)35)14(30)11(43-21)6-41-48(38,39)45-47(36,37)'
+                            '40-5-10-13(29)15(31)20(42-10)27-3-1-2-9(4-27)18(23)32'
+                            '/h1,3-4,7-8,10-11,13-16,20-21,29-31H,2,5-6H2,(H2,23,32)(H,36,37)(H,38,39)(H2,22,24,25)(H2,33,34,35)'
+                            '/t10-,11-,13-,14-,15-,16-,20-,21-/m1/s1')),
+                    coefficient=-1),
+                data_model.ReactionParticipant(
+                    specie=data_model.Specie(
+                        id='H+',
+                        structure='InChI=1S/p+1'),
+                    coefficient=-1),
+                data_model.ReactionParticipant(
+                    specie=data_model.Specie(
+                        id='1,2-Propanediol',
+                        structure='InChI=1S/C3H8O2/c1-3(5)2-4/h3-5H,2H2,1H3'),
+                    coefficient=1),
+                data_model.ReactionParticipant(
+                    specie=data_model.Specie(
+                        id='NADP+',
+                        structure=(
+                            'InChI=1S/C21H28N7O17P3'
+                            '/c22-17-12-19(25-7-24-17)28(8-26-12)21-16(44-46(33,34)35)14(30)11(43-21)6-41-48(38,39)'
+                            '45-47(36,37)40-5-10-13(29)15(31)20(42-10)27-3-1-2-9(4-27)18(23)32'
+                            '/h1-4,7-8,10-11,13-16,20-21,29-31H,5-6H2,(H7-,22,23,24,25,32,33,34,35,36,37,38,39)'
+                            '/t10-,11-,13-,14-,15-,16-,20-,21-/m1/s1')),
+                    coefficient=1),
+            ],
+            cross_references=[
+                data_model.Resource(namespace='ec-code', id='1.1.1.52', assignment_method=data_model.ResourceAssignmentMethod.manual),
+            ])
 
     def test_get_compounds_by_structure(self):
-        sabio_rk = self.sabio_rk
+        q = reaction_kinetics.ReactionKineticsQuery()
 
-        self.assertRaises(ValueError, sabio_rk.get_compounds_by_structure, '')
+        inchi = 'InChI=1S/C6H13O9P/c7-3-2(1-14-16(11,12)13)15-6(10)5(9)4(3)8/h2-10H,1H2,(H2,11,12,13)/t2-,3-,4+,5-,6+/m1/s1'
 
-        structure = 'InChI=1S/C6H13O9P/c7-3-2(1-14-16(11,12)13)15-6(10)5(9)4(3)8/h2-10H,1H2,(H2,11,12,13)/t2-,3-,4+,5-,6+/m1/s1'
-        compounds = sabio_rk.get_compounds_by_structure(structure,
-                                                        check_protonation=True,
-                                                        check_double_bonds=True,
-                                                        check_stereochemistry=True)
-        self.assertEqual(len(compounds), 1)
-        self.assertEqual(compounds[0].id, 24)
+        compounds = q.get_compounds_by_structure(inchi).all()
+        self.assertEqual(set(c.id for c in compounds), set([24, 1323, 1357, 1404, 1405, 1493, 1517, 1522, 24709]))
+        # 1517 is a match based on the InChI conversion of its SMILES representation
+        # 1524 is not in the sqlite database beacuse it it reachable from any of reaction
 
-        structure = 'InChI=1S/C6H13O9P/c7-3-2(1-14-16(11,12)13)15-6(10)5(9)4(3)8/h2-10H,1H2,(H2,11,12,13)/t2-,3-,4+,5-,6+/m1/s1'
-        compounds = sabio_rk.get_compounds_by_structure(structure)
-        self.assertEqual(len(compounds), 10)
-        self.assertEqual(set(c.id for c in compounds), set([24, 1323, 1357, 1366, 1404, 1405, 1493, 1522, 1524, 24709]))
+        compounds = q.get_compounds_by_structure(inchi, only_formula_and_connectivity=False).all()
+        self.assertEqual([c.id for c in compounds], [24])
+
+        # select=sabio_rk.Compound.id
+        compounds = q.get_compounds_by_structure(inchi, only_formula_and_connectivity=False, select=sabio_rk.Compound.id).all()
+        self.assertEqual([c[0] for c in compounds], [24])
+
+    def test_get_reactions_by_compound(self):
+        q = reaction_kinetics.ReactionKineticsQuery()
+
+        d_Lactaldehyde = 'InChI=1S/C3H6O2/c1-3(5)2-4/h2-3,5H,1H3/t3-/m1/s1'
+
+        # whole structure, reactant, select id
+        q_rxn = q.get_reactions_by_compound(
+            d_Lactaldehyde, only_formula_and_connectivity=False,
+            role='reactant', select=sabio_rk.Reaction.id) \
+            .order_by(sabio_rk.Reaction.id)
+        # reactions: [548, 10424], laws: [22877, 22870, 22882, 38452, 44597, 50464, 50469, 50470]
+        self.assertEqual([r[0] for r in q_rxn.all()], [548])
+
+        # whole structure, reactant, select object
+        q_rxn = q.get_reactions_by_compound(
+            d_Lactaldehyde, only_formula_and_connectivity=False,
+            role='reactant', select=sabio_rk.Reaction) \
+            .order_by(sabio_rk.Reaction.id)
+        # reactions: [548, 10424], laws: [22877, 22870, 22882, 38452, 44597, 50464, 50469, 50470]
+        self.assertEqual([r.id for r in q_rxn.all()], [548])
+
+        # formula and connectivity, reactant, select object
+        q_rxn = q.get_reactions_by_compound(
+            molecule_util.InchiMolecule(d_Lactaldehyde).get_formula_and_connectivity(), only_formula_and_connectivity=True,
+            role='reactant', select=sabio_rk.Reaction) \
+            .order_by(sabio_rk.Reaction.id)
+        rxn_ids = [r.id for r in q_rxn.all()]  # laws: [50464, 26317, 28508, 41400]
+        self.assertIn(548, rxn_ids)
+        self.assertIn(553, rxn_ids)
+        self.assertIn(554, rxn_ids)
+        self.assertIn(12492, rxn_ids)
+
+        # whole structure, product, select object
+        q_rxn = q.get_reactions_by_compound(
+            d_Lactaldehyde, only_formula_and_connectivity=False,
+            role='product', select=sabio_rk.Reaction) \
+            .order_by(sabio_rk.Reaction.id)
+        self.assertEqual([r.id for r in q_rxn.all()], [10424])  # reactions: [10424], laws: [44603]
+
+    def test_get_reactions_by_participants(self):
+        q = reaction_kinetics.ReactionKineticsQuery()
+
+        participants = self.reaction_1_1_1_55.participants
+
+        # only_formula_and_connectivity=True, include_water_hydrogen=False
+        reactions = q.get_reactions_by_participants(participants) \
+            .order_by(sabio_rk.Reaction.id)
+        self.assertEqual([r.id for r in reactions], [554, 2227, 10434])
+
+        # only_formula_and_connectivity=False
+        reactions = q.get_reactions_by_participants(participants, only_formula_and_connectivity=False) \
+            .order_by(sabio_rk.Reaction.id)
+        self.assertEqual([r.id for r in reactions], [554, 2227])
+
+        # include_water_hydrogen=True
+        reactions = q.get_reactions_by_participants(participants, include_water_hydrogen=True) \
+            .order_by(sabio_rk.Reaction.id)
+        self.assertEqual([r.id for r in reactions], [554, 2227, 10434])
+
+        # select=sabio_rk.Reaction.id
+        reactions = q.get_reactions_by_participants(participants, select=sabio_rk.Reaction.id) \
+            .order_by(sabio_rk.Reaction.id)
+        self.assertEqual([r[0] for r in reactions], [554, 2227, 10434])
+
+    def test_get_kinetic_laws_by_participants(self):
+        q = reaction_kinetics.ReactionKineticsQuery()
+
+        participants = self.reaction_1_1_1_55.participants
+
+        # only_formula_and_connectivity=True, include_water_hydrogen=False
+        laws = q.get_kinetic_laws_by_participants(participants) \
+            .order_by(sabio_rk.KineticLaw.id)
+        self.assertEqual([l.id for l in laws], [22871, 22874, 22876, 22880, 28503, 28508, 38455, 46425, 46426, 46427, 46428])
+
+        # only_formula_and_connectivity=False
+        laws = q.get_kinetic_laws_by_participants(participants, only_formula_and_connectivity=False) \
+            .order_by(sabio_rk.KineticLaw.id)
+        self.assertEqual([l.id for l in laws], [22871, 28508, 38455, 46425, 46426, 46427, 46428])
+
+        # include_water_hydrogen=True
+        laws = q.get_kinetic_laws_by_participants(participants, include_water_hydrogen=True) \
+            .order_by(sabio_rk.KineticLaw.id)
+        self.assertEqual([l.id for l in laws], [22871, 22874, 22876, 22880, 28503, 28508, 38455, 46425, 46426, 46427, 46428])
+
+        # select=sabio_rk.KineticLaw.id
+        laws = q.get_kinetic_laws_by_participants(participants, select=sabio_rk.KineticLaw.id) \
+            .order_by(sabio_rk.KineticLaw.id)
+        self.assertEqual([l[0] for l in laws], [22871, 22874, 22876, 22880, 28503, 28508, 38455, 46425, 46426, 46427, 46428])
+
+    def test_get_kinetic_laws_by_ec_numbers(self):
+        q = reaction_kinetics.ReactionKineticsQuery()
+
+        # single EC, match_levels=4
+        laws = q.get_kinetic_laws_by_ec_numbers(['1.1.1.52'], match_levels=4).all()
+        ids_52 = set([16011, 16013, 16016])
+        self.assertEqual(set([l.id for l in laws]), ids_52)
+
+        laws = q.get_kinetic_laws_by_ec_numbers(['1.1.1.55'], match_levels=4).all()
+        ids_55 = set([46425, 46426, 46427, 46428])
+        self.assertEqual(set([l.id for l in laws]), ids_55)
+
+        laws = q.get_kinetic_laws_by_ec_numbers(['1.1.1.59'], match_levels=4).all()
+        ids_59 = set([23931, 23932])
+        self.assertEqual(set([l.id for l in laws]), ids_59)
+
+        # multiple EC, match_levels=4
+        laws = q.get_kinetic_laws_by_ec_numbers(['1.1.1.52', '1.1.1.55'], match_levels=4).all()
+        self.assertEqual(set([l.id for l in laws]), ids_52 | ids_55)
+
+        laws = q.get_kinetic_laws_by_ec_numbers(['1.1.1.52', '1.1.1.59'], match_levels=4).all()
+        self.assertEqual(set([l.id for l in laws]), ids_52 | ids_59)
+
+        laws = q.get_kinetic_laws_by_ec_numbers(['1.1.1.55', '1.1.1.59'], match_levels=4).all()
+        self.assertEqual(set([l.id for l in laws]), ids_55 | ids_59)
+
+        laws = q.get_kinetic_laws_by_ec_numbers(['1.1.1.52', '1.1.1.55', '1.1.1.59'], match_levels=4).all()
+        self.assertEqual(set([l.id for l in laws]), ids_52 | ids_55 | ids_59)
+
+        # match_levels=3
+        laws = q.get_kinetic_laws_by_ec_numbers(['1.1.1.52'], match_levels=3).all()
+        ids = set([l.id for l in laws])
+        ids_contains = ids_52 | ids_55 | ids_59
+        self.assertGreater(len(ids), len(ids_contains))
+        self.assertEqual(ids_contains.difference(ids), set())
+
+        # match_levels=3, select=sabio_rk.KineticLaw.id
+        laws = q.get_kinetic_laws_by_ec_numbers(['1.1.1.52'], match_levels=3, select=sabio_rk.KineticLaw.id).all()
+        ids = set([l[0] for l in laws])
+        ids_contains = ids_52 | ids_55 | ids_59
+        self.assertGreater(len(ids), len(ids_contains))
+        self.assertEqual(ids_contains.difference(ids), set())
+
+    def test_get_kinetic_laws_by_reaction(self):
+        q = reaction_kinetics.ReactionKineticsQuery()
+
+        laws = q.get_kinetic_laws_by_reaction(self.reaction_1_1_1_55) \
+            .order_by(sabio_rk.KineticLaw.id) \
+            .all()
+        self.assertEqual([l.id for l in laws], [22871, 22874, 22876, 22880, 28503, 28508, 38455, 46425, 46426, 46427, 46428])
+
+        # select=sabio_rk.KineticLaw.id
+        laws = q.get_kinetic_laws_by_reaction(self.reaction_1_1_1_55, select=sabio_rk.KineticLaw.id) \
+            .order_by(sabio_rk.KineticLaw.id) \
+            .all()
+        self.assertEqual([l[0] for l in laws], [22871, 22874, 22876, 22880, 28503, 28508, 38455, 46425, 46426, 46427, 46428])
+
+        # get by EC
+        laws = q.get_kinetic_laws_by_reaction(self.reaction_not_1_1_1_52) \
+            .order_by(sabio_rk.KineticLaw.id) \
+            .all()
+        self.assertEqual([l.id for l in laws], [16011, 16013, 16016])
+
+    def test_get_observed_values(self):
+        q = reaction_kinetics.ReactionKineticsQuery()
+        vals = q.get_observed_values(self.reaction_1_1_1_55)
+
+        table = []
+        for v in vals:
+            row = [v.observable.name, None, None, v.value, v.units]
+            if v.observable.parent.parent:
+                row[1] = v.observable.parent.parent.name
+            if v.observable.parent.parent.parent:
+                row[2] = v.observable.parent.parent.parent.name
+            table.append(row)
+
+        print('\n')
+        print('{:<20}  {:<20}  {:<20}  {:<20}  {:<20}'.format('Parameter', 'Compound', 'Compartment', 'Value', 'Units'))
+        print('{:<20}  {:<20}  {:<20}  {:<20}  {:<20}'.format('=' * 20, '=' * 20, '=' * 20, '=' * 20, '=' * 20))
+        for row in table:
+            print('{:<20}  {:<20}  {:<20}  {:>20}  {:<20}'.format(row[0] or '', row[1] or '', row[2] or '', row[3] or '', row[4] or ''))
+
+        self.assertIn(['kcat_Km', 'N-Ethylmaleimide', None, 85000.0, None], table)
+
+    @unittest.skip('implement me')
+    def test_(self):
+        mol = data_model.Specie(structure=inchi)
+
+        ec_numbers = ['1.1.1.52', '1.1.1.55']
+        rxn = data_model.Reaction(cross_references=[
+            data_model.Resource(namespace='ec-code', id=ec_number),
+        ])
