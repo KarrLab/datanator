88e097af5b5af44ab59ebd65def7a1a24c1a4cb1
Jonathan Karr <jonrkarr@gmail.com>
2017-06-02 12:13:49 -0400

    debugging

--- kinetic_datanator/data_source/array_express.py
+++ kinetic_datanator/data_source/array_express.py
@@ -20,41 +20,44 @@ sample_characteristic = sqlalchemy.Table(
     sqlalchemy.Column('sample__id', sqlalchemy.Integer, sqlalchemy.ForeignKey('sample._id'), index=True),
     sqlalchemy.Column('characteristic_id', sqlalchemy.Integer, sqlalchemy.ForeignKey('characteristic._id'), index=True),
 )
-# :obj:`sqlalchemy.Table`: Sample:Characteristic one-to-many association table
+# :obj:`sqlalchemy.Table`: Sample:Characteristic many-to-many association table
 
 sample_variable = sqlalchemy.Table(
     'sample_variable', Base.metadata,
     sqlalchemy.Column('sample__id', sqlalchemy.Integer, sqlalchemy.ForeignKey('sample._id'), index=True),
-    sqlalchemy.Column('variable_id', sqlalchemy.Integer, sqlalchemy.ForeignKey('variable._id')),
+    sqlalchemy.Column('variable_id', sqlalchemy.Integer, sqlalchemy.ForeignKey('variable._id'), index=True),
 )
-# :obj:`sqlalchemy.Table`: Sample:Variable one-to-many association table
+# :obj:`sqlalchemy.Table`: Sample:Variable many-to-many association table
 
 
 class Characteristic(Base):
     """ Represents an expiremental characteristic
     Attributes:
+        samples
         name (:obj:`str`): name of the characteristic (e.g. organism)
         value (:obj:`str`): value of characteristic (e.g Mus musculus)
     """
     _id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True)
-    sample_id = sqlalchemy.ForeignKey('sample._id')
     name = sqlalchemy.Column(sqlalchemy.String())
     value = sqlalchemy.Column(sqlalchemy.String())
 
+    sqlalchemy.schema.UniqueConstraint(name, value)
 
     __tablename__ = 'characteristic'
+
 
 class Variable(Base):
     """ Represents an expiremental variable
     Attributes:
+        samples
         name (:obj:`str`): name of the variable (e.g. genotype)
-        value (:obj:`str`): value of characteristic (e.g control)
+        value (:obj:`str`): value of variable (e.g control)
     """
     _id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True)
-    sample_id = sqlalchemy.ForeignKey('sample._id')
     name = sqlalchemy.Column(sqlalchemy.String())
     value = sqlalchemy.Column(sqlalchemy.String())
 
+    sqlalchemy.schema.UniqueConstraint(name, value)
 
     __tablename__ = 'variable'
 
@@ -62,17 +65,38 @@ class Variable(Base):
 class Sample(Base):
     """ Represents an observed concentration
     Attributes:
-        assay_name (:obj:`str`): name of the particular assay in the experiment
+        experiment
+        index
+        name (:obj:`str`): name of the source of the sample
+        characteristics
+        variables
     """
     _id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True)
+    experiment_id = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey('experiment._id'), index=True)
+    experiment = sqlalchemy.orm.relationship('Experiment', backref=sqlalchemy.orm.backref('samples'), foreign_keys=[experiment_id])
+    index = sqlalchemy.Column(sqlalchemy.Integer())
+    name = sqlalchemy.Column(sqlalchemy.String())
+    characteristics = sqlalchemy.orm.relationship('Characteristic',
+                                                  secondary=sample_characteristic, backref=sqlalchemy.orm.backref('samples'))
+    variables = sqlalchemy.orm.relationship('Variable',
+                                            secondary=sample_variable, backref=sqlalchemy.orm.backref('samples'))
 
-    assay_name = sqlalchemy.Column(sqlalchemy.String())
+    sqlalchemy.schema.UniqueConstraint(experiment_id, index)
+    sqlalchemy.schema.UniqueConstraint(experiment_id, name)
 
-    characteristics = sqlalchemy.orm.relationship('Characteristic', secondary=sample_characteristic, backref=sqlalchemy.orm.backref('samples'))
-    variable = sqlalchemy.orm.relationship('Variable', secondary=sample_variable, backref=sqlalchemy.orm.backref('samples'))
+    __tablename__ = 'sample'
 
-    __tablename__ = 'samples'
 
+class Experiment(Base):
+    """
+    Attributes:
+        id
+        samples
+    """
+    _id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True)
+    id = sqlalchemy.Column(sqlalchemy.String(), unique=True)
+
+    __tablename__ = 'experiment'
 
 
 class ArrayExpress(data_source.HttpDataSource):
@@ -84,22 +108,18 @@ class ArrayExpress(data_source.HttpDataSource):
 
     base_model = Base
     ENDPOINT_DOMAIN = 'https://www.ebi.ac.uk/arrayexpress/xml/v3/experiments'
-    DOWNLOAD_SAMPLE_URL = ENDPOINT_DOMAIN +'/{}/samples'
-
-
-
+    DOWNLOAD_SAMPLE_URL = ENDPOINT_DOMAIN + '/{}/samples'
 
     def load_content(self):
         """ Download the content of SABIO-RK and store it to a local sqlite database. """
         db_session = self.session
         req_session = self.requests_session
 
-
         list_experiments = ['E-MTAB-5775']
 
         xml_parser = jxmlease.Parser()
         for i_entry, entry in enumerate(list_experiments):
-            
+
             sample = Sample()
 
             print self.DOWNLOAD_SAMPLE_URL.format(entry)
@@ -110,33 +130,18 @@ class ArrayExpress(data_source.HttpDataSource):
                 warnings.warn('Unable to download data for compound {}'.format(entry), data_source.DataSourceWarning)
                 continue
 
-            entry_details = xml_parser(response.text)#['compound']
+            entry_details = xml_parser(response.text)  # ['compound']
             print entry_details['experiment']['sample'][0]
             for thing in entry_details['experiment']['sample'][0]:
-            	print thing
+                print thing
 
             print entry_details['experiment']['sample'][0]['assay']['name']
             sample.assay_name = entry_details['experiment']['sample'][0]['assay']['name']
 
-
-            #for thing in entry_details['experiment']['sample']:
-            #	print thing
-
-
-
-
-
-
-
-
-
+            # for thing in entry_details['experiment']['sample']:
+            #   print thing
 
 
 if __name__ == '__main__':
-	blue = ArrayExpress()
-	blue.load_content()
-
-
-
-
-
+    blue = ArrayExpress()
+    blue.load_content()
