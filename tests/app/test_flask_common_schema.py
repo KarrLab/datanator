8c99b6e5dd6c8a0eab626e11b39b2100102317b9
saapooch <saahith116@gmail.com>
2018-01-25 15:07:14 -0500

    testing downloaded CS

--- tests/app/test_flask_common_schema.py
+++ tests/app/test_flask_common_schema.py
@@ -150,7 +150,6 @@ class LoadingTestFlaskCommonSchema(unittest.TestCase):
         self.assertEqual(set([c.name for c in models.Compound.query.whoosh_search('adenine').all()]),
             set(['Adenosine', 'Adenosine monophosphate', 'Cyclic AMP', "Adenosine 3',5'-diphosphate", 'Adenine']))
 
-@unittest.skip('Conflicting caches')
 class DownloadTestFlaskCommonSchema(unittest.TestCase):
 
     @classmethod
