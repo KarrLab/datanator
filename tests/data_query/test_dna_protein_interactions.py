d57affcce5f4eef6a298b8f3982f6d7c0bed5930
saapooch <saahith116@gmail.com>
2018-01-26 12:00:59 -0500

    dna_protein interaction update

--- tests/data_query/test_dna_protein_interactions.py
+++ tests/data_query/test_dna_protein_interactions.py
@@ -8,6 +8,7 @@
 
 from kinetic_datanator.core import data_model, common_schema
 from kinetic_datanator.data_query import dna_protein_interactions as dpi
+from kinetic_datanator.app import models, flask_common_schema
 import unittest
 from Bio import motifs
 import tempfile
@@ -23,7 +24,7 @@ class TestProteintoDNAInteractionQueryGenerator(unittest.TestCase):
     """
 
     @classmethod
-    def setUp(self):
+    def setUpClass(self):
         self.arnt  = data_model.ProteinSpecie(uniprot_id = 'P53762', gene_name = 'ARNT')
 
     def test_filter_observed_values(self):
@@ -52,7 +53,7 @@ class TestDNAtoProteinInteractionQueryGenerator(unittest.TestCase):
 
     """
     @classmethod
-    def setUp(self):
+    def setUpClass(self):
         self.dna_segment1 = data_model.DnaSpecie(sequence = 'CCTTTGTT')
         self.dna_segment2 = data_model.DnaSpecie(sequence = 'AAGGTCAA')
 
@@ -65,6 +66,65 @@ class TestDNAtoProteinInteractionQueryGenerator(unittest.TestCase):
 
     def test_get_protein_by_binding_matrix(self):
         q = dpi.DNAtoProteinInteractionQueryGenerator()
+
+        query = q.get_protein_by_DNA_sequence(self.dna_segment1.sequence)
+
+        self.assertEqual(set(c[0].subunit_name for c in query), set(['pan', 'Sox2', 'DOF5.6']))
+        self.assertEqual(set(c[1] for c in query), set([0,0,-8,-8]))
+
+class TestFlaskProteintoDNAInteractionQueryGenerator(unittest.TestCase):
+    """
+    Tests for Protein to DNA interactions
+
+    Case: You have a protein and want to find the DNA binding site
+
+    """
+
+    @classmethod
+    def setUpClass(self):
+        self.cache_dirname = tempfile.mkdtemp()
+        flk = flask_common_schema.FlaskCommonSchema(cache_dirname=self.cache_dirname)
+
+        self.arnt  = flk.session.query(models.ProteinSubunit).filter_by(uniprot_id = 'P53762').first()
+
+    def test_filter_observed_values(self):
+        q = dpi.FlaskProteintoDNAInteractionQueryGenerator()
+
+        observable = q.get_observed_values(self.arnt)
+
+        self.assertEqual(observable[0].specie.sequence, 'CACGTG')
+        self.assertEqual(observable[0].specie.cross_references[0].id, '7592839')
+
+
+    def test_get_DNA_by_protein(self):
+        q = dpi.FlaskProteintoDNAInteractionQueryGenerator()
+
+        position = q.get_DNA_by_protein(self.arnt)
+
+        self.assertEqual(set(c.frequency_a for c in position[0]), set([0,19,4]))
+        self.assertEqual(set(c.position for c in position[0]), set([1, 2, 3, 4, 5, 6]))
+
+class TestFlaskDNAtoProteinInteractionQueryGenerator(unittest.TestCase):
+    """
+    Tests for DNA to Protein interactions
+
+    Case: You have a DNA segment and want to find binding Protein
+
+    """
+    @classmethod
+    def setUpClass(self):
+        self.dna_segment1 = data_model.DnaSpecie(sequence = 'CCTTTGTT')
+        self.dna_segment2 = data_model.DnaSpecie(sequence = 'AAGGTCAA')
+
+    def test_filter_observed_values(self):
+        q = dpi.FlaskDNAtoProteinInteractionQueryGenerator()
+
+        observe = q.get_observed_values(self.dna_segment2)
+        self.assertEqual(set(c.specie.gene_name for c in observe), set(['NR4A2', 'TRP(MYB) class']))
+
+
+    def test_get_protein_by_binding_matrix(self):
+        q = dpi.FlaskDNAtoProteinInteractionQueryGenerator()
 
         query = q.get_protein_by_DNA_sequence(self.dna_segment1.sequence)
 
