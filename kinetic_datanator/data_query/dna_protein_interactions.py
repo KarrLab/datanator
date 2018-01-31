01c0816d1fd7592342b0f09d343660ab54bba14e
saapooch <saahith116@gmail.com>
2018-01-31 15:14:30 -0500

    deprecating pure SQLAlchemy common_schema

--- kinetic_datanator/data_query/dna_protein_interactions.py
+++ kinetic_datanator/data_query/dna_protein_interactions.py
@@ -5,7 +5,7 @@
 :License: MIT
 """
 
-from kinetic_datanator.core import data_model, data_query,  common_schema
+from kinetic_datanator.core import data_model, data_query
 from kinetic_datanator.app import models, flask_common_schema
 from Bio import motifs
 from Bio.Seq import Seq
@@ -38,198 +38,13 @@ class ProteintoDNAInteractionQueryGenerator(data_query.CachedDataSourceQueryGene
             taxon=taxon, max_taxon_dist=max_taxon_dist, taxon_dist_scale=taxon_dist_scale, include_variants=include_variants,
             temperature=temperature, temperature_std=temperature_std,
             ph=ph, ph_std=ph_std,
-            data_source=common_schema.CommonSchema())
-
-    def get_observed_values(self, protein):
-        """ Find the DNA binding motif for a given protein
-
-        Args:
-            protein (:obj:`data_model.ProteinSpecie`): Protein to find data for
-
-        Returns:
-            :obj:`list` of :obj:`data_model.Observable`: list of observables
-
-        """
-        versions = self.get_DNA_by_protein(protein)
-
-        index = 0
-        observable = []
-        for motif in versions:
-            binding_matrix = []
-            for position in motif.all():
-                binding_matrix.append([position.frequency_a, position.frequency_c, position.frequency_g, position.frequency_t])
-            binding_matrix = map(list, zip(*binding_matrix))
-            self.cache_dirname = tempfile.mkdtemp()
-            with open(self.cache_dirname+'/data.pfm', 'w') as pfm:
-                for items in binding_matrix:
-                    writer = csv.writer(pfm, delimiter = '\t')
-                    writer.writerow(items)
-
-            m = motifs.read(open(self.cache_dirname+'/data.pfm'), 'pfm')
-
-            dna_specie = data_model.DnaSpecie(binding_matrix = m.counts, sequence = str(m.counts.consensus))
-            interaction = data_model.Interaction(name = 'Transcription Factor DNA Binding Site')
-            observable.append(data_model.Observable(specie = dna_specie, interaction = interaction))
-
-            for position in motif.all():
-                observable[index].specie.cross_references = data_model.Resource(namespace ='pubmed',\
-                id = position.dataset._metadata.resource[0]._id),
-                break
-
-            shutil.rmtree(self.cache_dirname)
-            index += 1
-
-        return observable
-
-
-    def get_DNA_by_protein(self, protein, select = common_schema.DNABindingData):
-        """
-        Args:
-            specie (:obj:`data_model.ProteinSpecie`): species to find data for
-
-        Returns:
-            :obj:`list` of :obj:`sqlalchemy.orm.query.Query`: list of versions queries for DNA binding observed for similar proteins
-        """
-
-        q = []
-
-        condition = common_schema.ProteinSubunit.uniprot_id == protein.uniprot_id
-
-        versions = self.data_source.session.query(common_schema.DNABindingDataset).\
-            join((common_schema.ProteinSubunit, common_schema.DNABindingDataset.subunit)).filter(condition).all()
-
-        for version in range(1,len(versions)+1):
-            q.append(self.data_source.session.query(select).join((common_schema.DNABindingDataset, select.dataset))\
-                .filter(common_schema.DNABindingDataset.version == version)\
-                .join((common_schema.ProteinSubunit, common_schema.DNABindingDataset.subunit)).filter(condition))
-
-        return q
-
-
-
-class DNAtoProteinInteractionQueryGenerator(data_query.CachedDataSourceQueryGenerator):
-    """ Queries DNA reigons for relevant proteins """
-
-    def __init__(self,
-                 taxon=None, max_taxon_dist=None, taxon_dist_scale=None, include_variants=False,
-                 temperature=37., temperature_std=1.,
-                 ph=7.5, ph_std=0.3):
-        """
-        Args:
-            taxon (:obj:`str`, optional): target taxon
-            max_taxon_dist (:obj:`int`, optional): maximum taxonomic distance to include
-            taxon_dist_scale (:obj:`float`, optional): The scale of the taxonomic distance scoring distribution.
-                This determines how quickly the score falls to zero away from zero.
-            include_variants (:obj:`bool`, optional): if :obj:`True`, also include observations from mutant taxa
-            temperature (:obj:`float`, optional): desired temperature to search for
-            temperature_std (:obj:`float`, optional): how much to penalize observations from other temperatures
-            ph (:obj:`float`, optional): desired pH to search for
-            ph_std (:obj:`float`, optional): how much to penalize observations from other pHs
-        """
-        super(DNAtoProteinInteractionQueryGenerator, self).__init__(
-            taxon=taxon, max_taxon_dist=max_taxon_dist, taxon_dist_scale=taxon_dist_scale, include_variants=include_variants,
-            temperature=temperature, temperature_std=temperature_std,
-            ph=ph, ph_std=ph_std,
-            data_source=common_schema.CommonSchema())
-
-
-    def get_observed_values(self, DnaSpecie):
-        """ Find observed concentrations for the metabolite or similar metabolites
-
-        Args:
-            specie (:obj:`data_model.DnaSpecie`): species to find data for
-
-        Returns:
-            :obj:`list` of :obj:`data_model.Observable`: list of observable
-        """
-        proteins = self.get_protein_by_DNA_sequence(DnaSpecie.sequence)
-
-        observable = []
-        for subunits in proteins:
-
-            protein = data_model.ProteinSpecie(uniprot_id = subunits[0].uniprot_id,\
-                entrez_id = subunits[0].entrez_id, gene_name = subunits[0].gene_name,\
-                length = subunits[0].length, mass = subunits[0].mass)
-            protein.cross_references = []
-            for doc in subunits[0]._metadata.resource:
-                protein.cross_references.append(data_model.Resource(namespace = 'pubmed', id = doc._id))
-
-            interaction = data_model.Interaction(name = 'Transcription Factor DNA Binding Site', \
-                position = subunits[1], score = subunits[2])
-
-            observable.append(data_model.Observable(specie = protein, interaction = interaction))
-
-        return observable
-
-
-    def get_protein_by_DNA_sequence(self,sequence, select = common_schema.ProteinSubunit):
-        """
-        Args:
-            sequence (:obj:`data_model.DnaSpecie.sequence`): sequence of DNA segment
-
-        Returns:
-            :obj:`list` of :obj:`tuple`: Returns the query for a protein, sequence position, and score
-
-        """
-        #TODO: Make more efficient. Add gene location filter
-
-        ans = []
-
-        size = len(sequence)
-        all_matricies = self.data_source.session.query(common_schema.DNABindingDataset).all()
-        all_sequences = []
-        for matricies in all_matricies:
-            if len(matricies.dna_binding_data) == size:
-                binding_matrix = []
-                for position in matricies.dna_binding_data:
-                    binding_matrix.append([position.frequency_a, position.frequency_c, position.frequency_g, position.frequency_t])
-                binding_matrix = map(list, zip(*binding_matrix))
-                self.cache_dirname = tempfile.mkdtemp()
-                with open(self.cache_dirname+'/data.pfm', 'w') as pfm:
-                    for items in binding_matrix:
-                        writer = csv.writer(pfm, delimiter = '\t')
-                        writer.writerow(items)
-                m = motifs.read(open(self.cache_dirname+'/data.pfm'), 'pfm')
-                my_seq = Seq(sequence, IUPAC.unambiguous_dna)
-                ##TODO: Include selective threshold
-                # distribution = m.pssm.distribution().threshold_paster()
-                # print distribution.threshold_paster()
-                for position, score in m.pssm.search(my_seq, threshold = 2):
-                    ans.append((matricies.subunit, position, score))
-
-        return ans
-
-
-class FlaskProteintoDNAInteractionQueryGenerator(data_query.CachedDataSourceQueryGenerator):
-    """ Queries Proteins for their DNA """
-
-    def __init__(self,
-                 taxon=None, max_taxon_dist=None, taxon_dist_scale=None, include_variants=False,
-                 temperature=37., temperature_std=1.,
-                 ph=7.5, ph_std=0.3):
-        """
-        Args:
-            taxon (:obj:`str`, optional): target taxon
-            max_taxon_dist (:obj:`int`, optional): maximum taxonomic distance to include
-            taxon_dist_scale (:obj:`float`, optional): The scale of the taxonomic distance scoring distribution.
-                This determines how quickly the score falls to zero away from zero.
-            include_variants (:obj:`bool`, optional): if :obj:`True`, also include observations from mutant taxa
-            temperature (:obj:`float`, optional): desired temperature to search for
-            temperature_std (:obj:`float`, optional): how much to penalize observations from other temperatures
-            ph (:obj:`float`, optional): desired pH to search for
-            ph_std (:obj:`float`, optional): how much to penalize observations from other pHs
-        """
-        super(FlaskProteintoDNAInteractionQueryGenerator, self).__init__(
-            taxon=taxon, max_taxon_dist=max_taxon_dist, taxon_dist_scale=taxon_dist_scale, include_variants=include_variants,
-            temperature=temperature, temperature_std=temperature_std,
-            ph=ph, ph_std=ph_std,
             data_source=flask_common_schema.FlaskCommonSchema())
 
     def get_observed_values(self, protein):
         """ Find the DNA binding motif for a given protein
 
         Args:
-            protein (:obj:`data_model.ProteinSpecie`): Protein to find data for
+            protein (:obj:`models.ProteinSubunit`): protein subunit to find data for
 
         Returns:
             :obj:`list` of :obj:`data_model.Observable`: list of observables
@@ -270,7 +85,7 @@ class FlaskProteintoDNAInteractionQueryGenerator(data_query.CachedDataSourceQuer
     def get_DNA_by_protein(self, protein, select = models.DNABindingData):
         """
         Args:
-            specie (:obj:`data_model.ProteinSpecie`): species to find data for
+            protein (:obj:`models.ProteinSubunit`): protein subunit to find data for
 
         Returns:
             :obj:`list` of :obj:`sqlalchemy.orm.query.Query`: list of versions queries for DNA binding observed for similar proteins
@@ -290,7 +105,7 @@ class FlaskProteintoDNAInteractionQueryGenerator(data_query.CachedDataSourceQuer
 
         return q
 
-class FlaskDNAtoProteinInteractionQueryGenerator(data_query.CachedDataSourceQueryGenerator):
+class DNAtoProteinInteractionQueryGenerator(data_query.CachedDataSourceQueryGenerator):
     """ Queries DNA reigons for relevant proteins """
 
     def __init__(self,
@@ -309,7 +124,7 @@ class FlaskDNAtoProteinInteractionQueryGenerator(data_query.CachedDataSourceQuer
             ph (:obj:`float`, optional): desired pH to search for
             ph_std (:obj:`float`, optional): how much to penalize observations from other pHs
         """
-        super(FlaskDNAtoProteinInteractionQueryGenerator, self).__init__(
+        super(DNAtoProteinInteractionQueryGenerator, self).__init__(
             taxon=taxon, max_taxon_dist=max_taxon_dist, taxon_dist_scale=taxon_dist_scale, include_variants=include_variants,
             temperature=temperature, temperature_std=temperature_std,
             ph=ph, ph_std=ph_std,
@@ -318,6 +133,9 @@ class FlaskDNAtoProteinInteractionQueryGenerator(data_query.CachedDataSourceQuer
 
     def get_observed_values(self, DnaSpecie):
         """ Find observed concentrations for the metabolite or similar metabolites
+
+        NOTE: Currently there are no Gene objects in common schema models. When added this
+        query will be updated to input models.Gene and output data_model.ProteinSpecie
 
         Args:
             specie (:obj:`data_model.DnaSpecie`): species to find data for
@@ -347,6 +165,11 @@ class FlaskDNAtoProteinInteractionQueryGenerator(data_query.CachedDataSourceQuer
 
     def get_protein_by_DNA_sequence(self,sequence, select = models.ProteinSubunit):
         """
+
+        NOTE: Currently there are no Gene objects in common schema models. When added this
+        query will be updated to input models.Gene and output data_model.ProteinSpecie
+
+
         Args:
             sequence (:obj:`data_model.DnaSpecie.sequence`): sequence of DNA segment
 
--- tests/data_query/test_protein_protein_interactions.py
+++ tests/data_query/test_protein_protein_interactions.py
@@ -1,6 +1,6 @@
 
 import unittest
-from kinetic_datanator.core import data_model, common_schema
+from kinetic_datanator.core import data_model
 from kinetic_datanator.data_query import protein_protein_interactions as ppi
 from kinetic_datanator.app import models, flask_common_schema
 import tempfile
@@ -8,68 +8,7 @@ import shutil
 import unittest
 
 
-class TestProteinInteractionandComplexQueryGenerator(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(self):
-        self.protein_Q9CWF2 = data_model.ProteinSpecie(uniprot_id = 'Q9CWF2')
-        self.protein = data_model.ProteinSpecie(uniprot_id = 'P53622')
-
-    def get_observed_values(self):
-        pass
-
-    def test_get_observable_interactions_and_complex(self):
-        q= ppi.ProteinInteractionandComplexQueryGenerator()
-
-        interaction, complex = q.get_observable_interactions_and_complex(self.protein)
-
-        self.assertEqual(set([i.participant_a for i in interaction]),
-            set([u'uniprotkb:P41810', u'uniprotkb:P41811', u'uniprotkb:P53622',
-            u'uniprotkb:P33767', u'uniprotkb:P40509']))
-        self.assertEqual(complex[0].name,'COPI vesicle coat complex')
-
-
-
-    def test_get_interaction_by_subunit(self):
-        q= ppi.ProteinInteractionandComplexQueryGenerator()
-
-        ans = q.get_interaction_by_subunit('P49418').all()
-
-        self.assertEqual(set([c.site_a for c in ans]),set([u'binding-associated region:545-695(MINT-1523780)',
-        u'binding-associated region:846-854(MINT-8096500)', u'binding-associated region:626-695(MINT-8094602)',
-        u'binding-associated region:626-695(MINT-8094834)',
-        u'glutathione s tranferase tag:?-?(MINT-8096286)|binding-associated region:620-695(MINT-8096289)',
-        u'glutathione s tranferase tag:?-?(MINT-8096392)|binding-associated region:620-695(MINT-8096394)',
-        u'binding-associated region:626-695(MINT-8094740)', u'-',
-        u'binding-associated region:620-695(MINT-8095970)|glutathione s tranferase tag:?-?(MINT-8095968)',
-        u'binding-associated region:626-695(MINT-376295)', u'binding-associated region:626-695(MINT-8094630)',
-        u'binding-associated region:620-695(MINT-8096476)|glutathione s tranferase tag:?-?(MINT-8096479)',
-        u'binding-associated region:545-695(MINT-1523797)',
-        u'binding-associated region:620-695(MINT-8095931)|glutathione s tranferase tag:?-?(MINT-8095934)',
-        u'glutathione s tranferase tag:?-?(MINT-8095915)|binding-associated region:620-695(MINT-8095918)',
-        u'glutathione s tranferase tag:?-?(MINT-8096369)|binding-associated region:620-695(MINT-8096366)',
-        u'binding-associated region:626-695(MINT-8094654)', u'binding-associated region:626-695(MINT-8094709)']))
-
-    def test_get_known_complex_by_subunit(self):
-        q= ppi.ProteinInteractionandComplexQueryGenerator()
-
-        ans = q.get_known_complex_by_subunit(self.protein_Q9CWF2.uniprot_id).all()
-
-        self.assertEqual(len(ans), 1)
-        self.assertEqual(ans[0].complex_name, 'Profilin 1 complex')
-        self.assertEqual(ans[0].go_id, 'GO:0006461;GO:0006897;GO:0030036')
-
-    def test_get_subunits_by_known_complex(self):
-        q= ppi.ProteinInteractionandComplexQueryGenerator()
-
-        name = 'Succinyl-CoA synthetase, GDP-forming'
-
-        ans = q.get_subunits_by_known_complex(name).all()
-
-        self.assertEqual([c.uniprot_id for c in ans],
-            [u'O19069', u'P53590', u'P53597', u'Q96I99', u'Q9WUM5', u'Q9Z2I8'])
-
-class TestFlaskProteinInteractionandComplexQueryGenerator(unittest.TestCase):
+class ProteinInteractionandComplexQueryGenerator(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
@@ -88,25 +27,25 @@ class TestFlaskProteinInteractionandComplexQueryGenerator(unittest.TestCase):
         pass
 
     def test_get_observable_complex(self):
-        q= ppi.FlaskProteinInteractionandComplexQueryGenerator()
+        q= ppi.ProteinInteractionandComplexQueryGenerator()
         complex = q.get_observable_complex(self.protein)
         self.assertEqual(complex[0].name,'COPI vesicle coat complex')
 
     def test_get_observable_interactions(self):
-        q= ppi.FlaskProteinInteractionandComplexQueryGenerator()
+        q= ppi.ProteinInteractionandComplexQueryGenerator()
         interaction = q.get_observable_interactions(self.protein)
         self.assertEqual(set([i.participant_a for i in interaction]),
             set([u'uniprotkb:P41810', u'uniprotkb:P41811', u'uniprotkb:P53622',
             u'uniprotkb:P33767', u'uniprotkb:P40509']))
 
     def test_get_observable_subunits(self):
-        q= ppi.FlaskProteinInteractionandComplexQueryGenerator()
+        q= ppi.ProteinInteractionandComplexQueryGenerator()
         subunits = q.get_observable_subunits(self.rhino_complex)
         self.assertEqual(set([c.uniprot_id for c in subunits]), set(['Q9VIF5', 'Q7JXA8', 'Q9V629']))
 
 
     def test_get_interaction_by_subunit(self):
-        q= ppi.FlaskProteinInteractionandComplexQueryGenerator()
+        q= ppi.ProteinInteractionandComplexQueryGenerator()
 
         ans = q.get_interaction_by_subunit('P49418').all()
 
@@ -126,7 +65,7 @@ class TestFlaskProteinInteractionandComplexQueryGenerator(unittest.TestCase):
         u'binding-associated region:626-695(MINT-8094654)', u'binding-associated region:626-695(MINT-8094709)']))
 
     def test_get_known_complex_by_subunit(self):
-        q= ppi.FlaskProteinInteractionandComplexQueryGenerator()
+        q= ppi.ProteinInteractionandComplexQueryGenerator()
 
         ans = q.get_known_complex_by_subunit(self.protein_Q9CWF2.uniprot_id).all()
 
@@ -135,7 +74,7 @@ class TestFlaskProteinInteractionandComplexQueryGenerator(unittest.TestCase):
         self.assertEqual(ans[0].go_id, 'GO:0006461;GO:0006897;GO:0030036')
 
     def test_get_subunits_by_known_complex(self):
-        q= ppi.FlaskProteinInteractionandComplexQueryGenerator()
+        q= ppi.ProteinInteractionandComplexQueryGenerator()
 
         name = 'Succinyl-CoA synthetase, GDP-forming'
 
