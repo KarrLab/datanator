7c81b144f46998de8b6894fb0c3d814b3bbccef4
yosefdroth <yosefdroth@gmail.com>
2018-02-09 09:43:04 -0500

    fixing errors

--- kinetic_datanator/core/flask_common_schema.py
+++ kinetic_datanator/core/flask_common_schema.py
@@ -252,37 +252,80 @@ class FlaskCommonSchema(data_source.HttpDataSource):
         total_experiments = ae.session.query(array_express.Experiment).all()
         print(total_experiments)
         total_samples = ae.session.query(array_express.Sample).all()
-        for sample in total_samples:
-            m_name = "RNA-Seq Sample Info: " + sample.experiment_id + "_" + sample.name #{}_{}".format(sample.experiment_id, s_name)
-            metadata = self.get_or_create_object(
-                models.Metadata,
-                name = m_name
-                )
-
-            metadata.characteristic = [
-                self.get_or_create_object(models.Characteristic, 
-                    category = characteristic.category, 
-                    value = characteristic.value) 
-                for characteristic in sample.characteristics
-                ]
-            
-            metadata.variable = [
-                self.get_or_create_object(models.Variable, 
-                    category = variable.name, 
-                    value = variable.value,
-                    units = variable.unit) 
-                for variable in sample.variables
-                ]
-
-            flask_sample = self.get_or_create_object(
-                models.RNASeqDataSet,
-                experiment_id = sample.experiment_id,
-                sample_name = sample.name,
-                assay = sample.assay,
-                ensembl_organism_strain = sample.ensembl_organism_strain,
-                read_type = sample.read_type,
-                full_strain_specificity = sample.full_strain_specificity
+        
+        
+        total_experiments = ae.session.query(array_express.Experiment).all()
+        for exp in total_experiments:
+            flask_experiment = self.get_or_create_object(
+                models.RNASeqExperiment,
+                accesion_number = exp.id
             )
+            for sample in total_samples:
+                #m_name = "RNA-Seq Sample Info: " + sample.experiment_id + "_" + sample.name #{}_{}".format(sample.experiment_id, s_name)
+                metadata = self.get_or_create_object(
+                    models.Metadata,
+                    name = "RNA-Seq Sample Info: " + sample.experiment_id + "_" + sample.name
+                    )
+
+                metadata.characteristic = [
+                    self.get_or_create_object(models.Characteristic, 
+                        category = characteristic.category, 
+                        value = characteristic.value) 
+                    for characteristic in sample.characteristics
+                    ]
+                
+                metadata.variable = [
+                    self.get_or_create_object(models.Variable, 
+                        category = variable.name, 
+                        value = variable.value,
+                        units = variable.unit) 
+                    for variable in sample.variables
+                    ]
+
+                flask_sample = self.get_or_create_object(
+                    models.RNASeqDataSet,
+                    experiment_accession_number = sample.experiment_id,
+                    sample_name = sample.name,
+                    assay = sample.assay,
+                    ensembl_organism_strain = sample.ensembl_organism_strain,
+                    read_type = sample.read_type,
+                    full_strain_specificity = sample.full_strain_specificity
+                )
+                flask_experiment.samples.append(flask_sample)
+            metadata = self.get_or_create_object(
+                    models.Metadata,
+                    name = "RNA-Seq Experiment Info: " + exp.id,
+                    description = exp.description
+                    )
+            metadata.taxon = [
+            self.get_or_create_object(
+                models.Taxon,
+                name = org.name
+                )
+            for org in exp.organisms]
+
+            metadata.resource.append(self.get_or_create_object(
+                models.Resource, 
+                namespace = "ArrayExpress", 
+                _id = exp.id,
+                release_date = exp.release_date
+                ))
+
+            metadata.method = [
+            self.get_or_create_object(
+                models.Method,
+                name = protocol.protocol_type,
+                comments = protocol.text,
+                performer = protocol.performer,
+                hardware = protocol.hardware,
+                software = protocol.software,
+                )
+            for protocol in exp.protocols]
+
+
+        if self.verbose:
+            print('Comitting1')
+        self.session.commit()
 
     def add_sabiodb(self):
         """
