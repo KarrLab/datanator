807053e7b67cde209c04e1c5efff15516a41640e
saapooch <saahith116@gmail.com>
2018-02-13 14:04:25 -0500

    revert

--- kinetic_datanator/util/molecule_util.py
+++ kinetic_datanator/util/molecule_util.py
@@ -49,24 +49,22 @@ class Molecule(object):
         """
         mol = openbabel.OBMol()
         obConversion = openbabel.OBConversion()
-        if self.structure:
-            if len(self.structure) >= 7 and self.structure[0:6] == 'InChI=' and \
-                    obConversion.SetInFormat('inchi') and \
-                    obConversion.ReadString(mol, self.structure):
-                return 'inchi'
-            elif obConversion.SetInFormat('can') and \
-                    obConversion.ReadString(mol, self.structure):
-                return 'can'
-            elif obConversion.SetInFormat('smiles') and \
-                    obConversion.ReadString(mol, self.structure):
-                return 'smiles'
-            elif obConversion.SetInFormat('mol') and \
-                    obConversion.ReadString(mol, self.structure):
-                return 'mol'
-            else:
-                return None
+        if len(self.structure) >= 7 and self.structure[0:6] == 'InChI=' and \
+                obConversion.SetInFormat('inchi') and \
+                obConversion.ReadString(mol, self.structure):
+            return 'inchi'
+        elif obConversion.SetInFormat('can') and \
+                obConversion.ReadString(mol, self.structure):
+            return 'can'
+        elif obConversion.SetInFormat('smiles') and \
+                obConversion.ReadString(mol, self.structure):
+            return 'smiles'
+        elif obConversion.SetInFormat('mol') and \
+                obConversion.ReadString(mol, self.structure):
+            return 'mol'
         else:
             return None
+
 
     def get_fingerprint(self, type='fp2'):
         """ Calculate a fingerprint
