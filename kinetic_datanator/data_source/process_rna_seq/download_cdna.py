8bf347a8a29a590262b418a08a2478fc3bc0de02
yosefdroth <yosefdroth@gmail.com>
2018-04-04 14:59:45 -0400

    refactoring for minerva

--- kinetic_datanator/data_source/process_rna_seq/download_cdna.py
+++ kinetic_datanator/data_source/process_rna_seq/download_cdna.py
@@ -2,8 +2,7 @@ from six.moves.urllib.request import urlretrieve
 import os
 import shutil
 
-
-def run(sample, top_dir):
+def run(ensembl_info, top_dir):
     """Downloads the CDNA for a given sample, and creates a kallisto index file. 
             The CDNA file is stored in a "CDNA" subdirectory within the top directory. 
             The kalliso index files are stored within "kallisto_index_files" subdirectory within the top directory
@@ -13,12 +12,13 @@ def run(sample, top_dir):
             top_dirname(:obj:`str`): the name of the directory where the overall data is being stored
 
         """
+
     DIRNAME = "{}/CDNA_FILES".format(top_dir)
     if not os.path.isdir(DIRNAME):
         os.makedirs(DIRNAME)
-    spec_name = sample.ensembl_info[0].organism_strain
+    spec_name = ensembl_info.organism_strain
     file_name = "{}/{}.cdna.all.fa.gz".format(DIRNAME, spec_name)
-    url = sample.ensembl_info[0].url
+    url = ensembl_info.url
     if not os.path.isfile(file_name):
         file = urlretrieve(url, '{}/{}.cdna.all.fa.gz'.format(top_dir, spec_name))
         shutil.move('{}/{}.cdna.all.fa.gz'.format(top_dir, spec_name), DIRNAME)
@@ -28,5 +28,5 @@ def run(sample, top_dir):
         os.makedirs(KALLISTO_DIR)
     if not os.path.isfile("{}/{}.idx".format(KALLISTO_DIR, spec_name)):
         os.system("kallisto index -i {}.idx {}".format(spec_name, file_name))
-        shutil.move("{}/{}.idx".format(top_dir, spec_name), KALLISTO_DIR)
+        shutil.move("{}/{}.idx".format(top_dir, spec_name), KALLISTO_
         
