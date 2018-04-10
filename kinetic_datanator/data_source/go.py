2690b32f744719324cb6e1dd332617665172cbe2
Saahith Pochiraju <saahith116@gmail.com>
2018-04-09 20:34:36 -0400

    go ontologies and tests

--- kinetic_datanator/data_source/go.py
+++ kinetic_datanator/data_source/go.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine, ForeignKey, exists, Column, Integer, Strin
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref, sessionmaker
 from kinetic_datanator.core import data_source
-
+import os
+import networkx
+import obonet
 
 Base = declarative_base()
 
@@ -14,9 +16,27 @@ class GO(data_source.HttpDataSource):
     """
 
     base_model = Base
-    ENDPOINT_DOMAINS = {
-        'GO':
-    }
+    ENDPOINT_DOMAINS = {'GO_obo': 'http://purl.obolibrary.org/obo/go.obo'}
+
 
     def load_content(self):
-        pass
+
+        #NOTE: Estimated time for loading the graph is 150 seconds
+        if os.path.isfile(self.cache_dirname+'/go.pkl'):
+            self.graph = self.get_graph()
+        else:
+            self.graph = self.generate_graph()
+            self.cache_graph(self.graph)
+
+
+    def get_name(self, graph, id):
+        return graph.nodes(data=True)[id]['name']
+
+    def generate_graph(self):
+        return obonet.read_obo(self.ENDPOINT_DOMAINS['GO_obo'])
+
+    def cache_graph(self, graph):
+        networkx.write_gpickle(graph, self.cache_dirname+'/go.pkl')
+
+    def get_graph(self):
+        return networkx.read_gpickle(self.cache_dirname+'/go.pkl')
