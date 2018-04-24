922b7ce59aed5a8cdc714be9e2406c3049a4ec18
saapooch <saahith116@gmail.com>
2018-04-24 15:01:40 -0400

    more extensive tests for protein abundance

--- tests/data_query/test_protein_abundance.py
+++ tests/data_query/test_protein_abundance.py
@@ -31,14 +31,34 @@ class TestProteinAbundanceQuery(unittest.TestCase):
     def test_filter_observed_results(self):
 
         vals = self.q.get_observed_result(self.protein_P00323)
+        self.assertEqual(set(items.value for items in vals), set([1003.0, 1100.0, 2323.0, 1336.0, 639.0]))
+        self.assertEqual(set(items.units for items in vals), set(['PPM']))
 
         for items in vals:
-            self.assertEqual(items.units, 'PPM')
+            self.assertEqual(items.observable.specie.name, self.protein_P00323.subunit_name)
+            self.assertEqual(items.observable.specie.uniprot_id, self.protein_P00323.uniprot_id)
+            self.assertEqual(items.observable.specie.entrez_id, self.protein_P00323.entrez_id)
+            self.assertEqual(items.observable.specie.gene_name, self.protein_P00323.gene_name)
+            self.assertEqual(items.observable.specie.length, self.protein_P00323.length)
+            self.assertEqual(items.observable.specie.mass, self.protein_P00323.mass)
             self.assertEqual(items.observable.specie.sequence, self.protein_P00323.canonical_sequence)
             if items.observable.specie.cross_references[0].id == '882/882-Desulfo_Form_Exp_SC_zhang_2006.txt':
                 self.assertEqual(items.value, 1003.0)
 
+        vals = self.q.get_observed_result(self.protein_Q42025)
+        self.assertEqual(set(items.value for items in vals), set([240.0, 140.0, 358.0]))
+        self.assertEqual(set(items.units for items in vals), set(['PPM']))
 
+        for items in vals:
+            self.assertEqual(items.observable.specie.name, self.protein_Q42025.subunit_name)
+            self.assertEqual(items.observable.specie.uniprot_id, self.protein_Q42025.uniprot_id)
+            self.assertEqual(items.observable.specie.entrez_id, self.protein_Q42025.entrez_id)
+            self.assertEqual(items.observable.specie.gene_name, self.protein_Q42025.gene_name)
+            self.assertEqual(items.observable.specie.length, self.protein_Q42025.length)
+            self.assertEqual(items.observable.specie.mass, self.protein_Q42025.mass)
+            self.assertEqual(items.observable.specie.sequence, self.protein_Q42025.canonical_sequence)
+            if items.observable.specie.cross_references[0].id == '3702/3702-2.1ArabidopsisLeaf_waterDefict_SOW.txt':
+                self.assertEqual(items.value, 140.0)
 
 
 
