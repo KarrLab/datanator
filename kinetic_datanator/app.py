9571aefd77734cf72eb122e6d0e9ca3d5a3eeadb
Saahith Pochiraju <saahith116@gmail.com>
2018-08-07 16:29:05 -0400

    include error pages

--- kinetic_datanator/app.py
+++ kinetic_datanator/app.py
@@ -62,9 +62,9 @@ def create_app():
     # def forbidden_page(error):
     #     return render_template('errors/403.html'), 403
     #
-    # @app.errorhandler(404)
-    # def page_not_found(error):
-    #     return render_template('errors/404.html'), 404
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('errors/404.html'), 404
     #
     # @app.errorhandler(500)
     # def server_error_page(error):
