bbaf60fc10cc94d8e55279aab533e4a7aeabb3c1
Jonathan Karr <jonrkarr@gmail.com>
2018-08-15 20:12:42 -0400

    simplifying UniProt source and adding snapshot to Quilt

--- kinetic_datanator/data_source/uniprot.py
+++ kinetic_datanator/data_source/uniprot.py
@@ -1,11 +1,23 @@
+""" Downloads and parses the UnitProt database for protein-protein interactions
+
+:Author: Saahith Pochiraju <saahith116@gmail.com>
+:Author: Jonathan Karr <jonrkarr@gmail.com>
+:Date: 2018-08-15
+:Copyright: 2017-2018, Karr Lab
+:License: MIT
+"""
+
 from kinetic_datanator.core import data_source
-import sqlalchemy.ext.declarative
-import pandas as pd
 from sqlalchemy import Column, Integer, String, Float
+import io
+import math
+import os
+import pandas
+import sqlalchemy.ext.declarative
 
 
 Base = sqlalchemy.ext.declarative.declarative_base()
-test = True
+
 
 class UniprotData(Base):
     """ Represents protein interactions in from the IntAct Database
@@ -24,18 +36,17 @@ class UniprotData(Base):
     """
 
     __tablename__ = 'uniprot'
-    index = Column(Integer, primary_key = True)
-    uniprot_id = Column(String(255), unique = True)
+    index = Column(Integer, primary_key=True)
+    uniprot_id = Column(String(255), unique=True)
     entry_name = Column(String(255))
     gene_name = Column(String(255))
     protein_name = Column(String(255))
     canonical_sequence = Column(String(255))
     length = Column(Integer)
-    mass = Column(String(255))
+    mass = Column(Integer)
     ec_number = Column(String(255))
     entrez_id = Column(Integer)
-    status  = Column(String(255))
-
+    status = Column(String(255))
 
 
 class Uniprot(data_source.HttpDataSource):
@@ -43,38 +54,31 @@ class Uniprot(data_source.HttpDataSource):
 
     """
     base_model = Base
-    ENDPOINT_DOMAINS = {'uniprot' : 'http://www.uniprot.org/uniprot/?query=*&fil=reviewed%3Ayes#',
-                        'uniprot-test': 'http://www.uniprot.org/uniprot/?sort=&desc=&compress=no&query=&fil=organism:%22Rattus%20norvegicus%20(Rat)%20[10116]%22%20AND%20reviewed:yes&limit=10&force=no&preview=true&format=tab&columns=id,entry%20name,genes(PREFERRED),protein%20names,sequence,length,mass,ec,database(GeneID),reviewed'}
-
+    ENDPOINT_DOMAINS = {
+        'uniprot': 'http://www.uniprot.org/uniprot/?fil=reviewed:yes',
+    }
 
     def load_content(self):
-
-        if test:
-            self.write_data_to_txt()
-            pand = pd.read_csv(self.cache_dirname+'/uniprot-test.txt', delimiter = '\t')
-        else:
-            pand = pd.read_csv(self.cache_dirname+'/uniprot-all.txt', delimiter = '\t')
-
-        new_columns = ['uniprot_id', 'entry_name', 'gene_name', 'protein_name', 'canonical_sequence', 'length', 'mass',
-            'ec_number', 'entrez_id', 'status']
-        pand.columns = new_columns
-
-        pand['entrez_id'] = pand['entrez_id'].str.replace(';', '')
-
-        if not self.max_entries == float('inf'):
-            pand = pand[0:self.max_entries]
-
-        pand.to_sql(name = 'uniprot', con=self.engine, if_exists = 'append', chunksize = 1000)
-        self.session.commit()
-
-
-    def write_data_to_txt(self):
-
-        database_url = self.ENDPOINT_DOMAINS['uniprot-test']
-        req = self.requests_session
-        response = req.get(database_url)
+        # download data
+        url = self.ENDPOINT_DOMAINS['uniprot']
+        url += '&columns=id,entry name,genes(PREFERRED),protein names,sequence,length,mass,ec,database(GeneID),reviewed'
+        url += '&format=tab'
+        url += '&compress=no'
+        if not math.isnan(self.max_entries):
+            url += '&limit={}'.format(self.max_entries)
+        response = self.requests_session.get(url)
         response.raise_for_status()
-        content = str(response.content.decode('utf-8')).split('\n')
-        with open(self.cache_dirname+'/uniprot-test.txt', 'w') as text_file:
-            for line in content:
-                text_file.write(line+'\n')
+
+        # parse data
+        table = pandas.read_csv(io.BytesIO(response.content), delimiter='\t', encoding='utf-8')
+
+        # put data into SQLite database
+        table.columns = [
+            'uniprot_id', 'entry_name', 'gene_name', 'protein_name', 'canonical_sequence', 'length', 'mass',
+            'ec_number', 'entrez_id', 'status',
+        ]
+        table['entrez_id'] = table['entrez_id'].str.replace(';', '')
+        table['mass'] = table['mass'].str.replace(',', '')
+
+        table.to_sql(name='uniprot', con=self.engine, if_exists='append', chunksize=1000)
+        self.session.commit()
--- tests/data_source/test_uniprot.py
+++ tests/data_source/test_uniprot.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 
-""" Test of common schema
+""" Test of UniProt module
 
 :Author: Saahith Pochiraju <saahith116@gmail.com>
-:Date: 2017-07-31
-:Copyright: 2017, Karr Lab
+:Author: Jonathan Karr <jonrkarr@gmail.com>
+:Date: 2018-08-15
+:Copyright: 2017-2018, Karr Lab
 :License: MIT
 """
-import unittest
 from kinetic_datanator.data_source import uniprot
-import tempfile
 import shutil
+import tempfile
+import unittest
 
 
 class TestServerDownloadUniprot(unittest.TestCase):
@@ -18,45 +19,49 @@ class TestServerDownloadUniprot(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         self.cache_dirname = tempfile.mkdtemp()
-        self.uni = uniprot.Uniprot(cache_dirname=self.cache_dirname)
-
-    @classmethod
-    def tearDownClass(self):
-        shutil.rmtree(self.cache_dirname)
-
-    def test_load(self):
-        test = self.uni.session.query(uniprot.UniprotData).filter_by(uniprot_id = 'B5F7M5').all()
-        self.assertEqual(len(test),1)
-        self.assertEqual(test[0].entry_name, 'AAEA_SALA4')
-        self.assertEqual(test[0].protein_name,
-        'p-hydroxybenzoic acid efflux pump subunit AaeA (pHBA efflux pump protein A)')
-
-    def test_load1(self):
-        test = self.uni.session.query(uniprot.UniprotData).filter_by(uniprot_id = 'Q39RX3').all()
-        self.assertEqual(len(test),1)
-        self.assertEqual(test[0].mass, '41,582')
-        self.assertEqual(test[0].ec_number, '2.3.1.31')
-
-    def test_load2(self):
-        test = self.uni.session.query(uniprot.UniprotData).filter_by(uniprot_id = 'A6USA0').all()
-        self.assertEqual(len(test),1)
-        self.assertEqual(test[0].entrez_id, 5324858)
-        self.assertEqual(test[0].ec_number, '2.1.1.166')
-
-class TestLoadingUniprot(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(self):
-        self.cache_dirname = tempfile.mkdtemp()
-        self.uni = uniprot.Uniprot(cache_dirname=self.cache_dirname, download_backups=False, load_content=True)
+        self.uni = uniprot.Uniprot(cache_dirname=self.cache_dirname, download_backups=False, load_content=True,
+                                   max_entries=10)
 
     @classmethod
     def tearDownClass(self):
         shutil.rmtree(self.cache_dirname)
 
     def test_proper_loading(self):
-        all_data = self.uni.session.query(uniprot.UniprotData).all()
-        self.assertEqual(len(all_data), 10)
-        self.assertTrue(all_data[0].uniprot_id)
-        self.assertTrue(all_data[0].mass)
-        self.assertTrue(all_data[8].uniprot_id)
+        count = self.uni.session.query(uniprot.UniprotData).count()
+        self.assertEqual(count, 10)
+
+    def test_load(self):
+        prot = self.uni.session.query(uniprot.UniprotData).filter_by(uniprot_id='Q12181').all()
+        self.assertEqual(len(prot), 1)
+        self.assertEqual(prot[0].entry_name, 'NDOR1_YEAST')
+        self.assertEqual(prot[0].protein_name,
+                         'NADPH-dependent diflavin oxidoreductase 1 (EC 1.18.1.-) (NADPH-dependent FMN and FAD-containing oxidoreductase)')
+        self.assertEqual(prot[0].entrez_id, 856161)
+
+    def test_load1(self):
+        prot = self.uni.session.query(uniprot.UniprotData).filter_by(uniprot_id='Q8GHV6').all()
+        self.assertEqual(len(prot), 1)
+        self.assertEqual(prot[0].mass, 37570)
+        self.assertEqual(prot[0].ec_number, None)
+
+    def test_load2(self):
+        prot = self.uni.session.query(uniprot.UniprotData).filter_by(uniprot_id='B5BAY2').all()
+        self.assertEqual(len(prot), 1)
+        self.assertEqual(prot[0].entrez_id, None)
+        self.assertEqual(prot[0].ec_number, '2.7.4.6')
+
+
+class TestUniprotFromBackup(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.cache_dirname = tempfile.mkdtemp()
+        self.uni = uniprot.Uniprot(cache_dirname=self.cache_dirname, download_backups=True)
+
+    @classmethod
+    def tearDownClass(self):
+        shutil.rmtree(self.cache_dirname)
+
+    def test_proper_loading(self):
+        count = self.uni.session.query(uniprot.UniprotData).count()
+        self.assertGreater(count, 500000)
