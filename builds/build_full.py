45ca254ec7126d4934f198f8e8d562e379287acc
saapooch <saahith116@gmail.com>
2018-09-10 16:08:06 -0400

    build scripts

---
+++ builds/build_full.py
@@ -0,0 +1,2 @@
+from kinetic_datanator.core import common_schema
+cs = common_schema.CommonSchema(load_content=True, clear_content=True, verbose=True, load_entire_small_DBs=True)
---
+++ builds/build_med.py
@@ -0,0 +1,2 @@
+from kinetic_datanator.core import common_schema
+cs = common_schema.CommonSchema(load_content=True, clear_content=True, verbose=True, max_entries=20, load_entire_small_DBs=True)
