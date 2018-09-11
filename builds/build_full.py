fdfce7d50c8736dc12ceb60a2f5bf08e5b1f46c4
saapooch <saahith116@gmail.com>
2018-09-11 10:49:49 -0400

    add upload

--- builds/build_full.py
+++ builds/build_full.py
@@ -1,2 +1,4 @@
 from kinetic_datanator.core import common_schema
 cs = common_schema.CommonSchema(load_content=True, clear_content=True, verbose=True, load_entire_small_DBs=True)
+cs.dump_database()
+cs.upload_backup()
