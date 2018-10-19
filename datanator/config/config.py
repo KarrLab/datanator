b3aa8ae39be780a2b408c8717bd294fdbe7eccd1
saapooch <saahith116@gmail.com>
2018-10-19 11:59:36 -0400

    mask production server

--- datanator/config/config.py
+++ datanator/config/config.py
@@ -1,5 +1,9 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+if os.path.isfile(os.path.join(basedir, '..', 'production_server.py')):
+    from datanator.production_server import PROD_SERVER_URI
+else:
+    PROD_SERVER_URI = 'postgres://postgres@localhost/CommonSchema'
 
 
 class BaseConfig(object):
@@ -50,15 +54,5 @@ class ProductionConfig(BaseConfig):
     BCRYPT_LOG_ROUNDS = 4
     WTF_CSRF_ENABLED = False
     #TODO: Need to determine the URI
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://karrlab:karr7116@commonschema.chqeewx1anny.us-east-1.rds.amazonaws.com/commonschema'
-    DEBUG_TB_ENABLED = False
+    SQLALCHEMY_DATABASE_URI = PROD_SERVER_URI
     PRESERVE_CONTEXT_ON_EXCEPTION = False
-
-# class ProductionConfig(BaseConfig):
-#     """Production configuration."""
-#     SECRET_KEY = 'my_precious'
-#     DEBUG = False
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://karrlab:karr7116@CommonSchema.chqeewx1anny.us-east-1.rds.amazonaws.com/CommonSchema'
-#     DEBUG_TB_ENABLED = False
-#     WTF_CSRF_ENABLED = True
-#     TESTING = False
