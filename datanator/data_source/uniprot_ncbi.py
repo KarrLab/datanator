476629d1bb20847f3a360afce93d47b71ec3f823
lzy7071 <zhouyang.lian@familian.life>
2019-01-28 11:41:23 -0500

    uniprot_ncbi.py store into dataframe

--- datanator/data_source/uniprot_ncbi.py
+++ datanator/data_source/uniprot_ncbi.py
@@ -3,31 +3,76 @@ Downloads and parse the latest release for
 ortholog -> uniprot mappings from omabrowser.org
 """
 
-import panda as pd
+import pandas as pd
 from time import time
 from datetime import datetime
+import sqlalchemy
 from sqlalchemy import Column, Integer, Float, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import urllib.request
 
-file_name = ["oma-uniprot", "oma-gi"]
+Base = sqlalchemy.ext.declarative.declarative_base()
+
+class Uniprot(Base):
+    """ Represents the oma-uniprot association table
+
+    Attributes:
+        oma (:obj:`str`): oma id
+        uniprot (:obj:`list`): uniprot id
+    """
+    _id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True)
+
+    oma = sqlalchemy.Column(sqlalchemy.String(), unique=True, index=True)
+
+    uniprot = sqlalchemy.Column(sqlalchemy.String(), unique=True, index=True)
+
+    __tablename__ = 'uniprot'
+
+class Ncbi(Base):
+    """ Represents the oma-ncbi association table
+
+    Attributes:
+        oma (:obj:`str`): oma id
+        ncbi (:obj:`list'): ncbi id
+    """
+    _id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True)
+
+    oma = sqlalchemy.Column(sqlalchemy.String(), unique=True, index=True)
+
+    ncbi = sqlalchemy.Column(sqlalchemy.String(), unique=True, index=True)
+
+    __tablename__ = 'ncbi'
+
+
+def download_data(file_name, file_type, root_folder):
+	print('Beginning file download with urllib: Downloading  ' + file_name)
+	BASE_URL = "https://omabrowser.org/All/"
+	#read in gzip files
+	# for file in file_name:
+	download_url = BASE_URL + file_name + file_type
+	storage_location = root_folder + file_name + file_type
+	response = urllib.request.urlretrieve(download_url, storage_location)
+
+	return response
+
+#load files into separate dataframes
+def load_data(file_name, file_tpye, root_folder):
+	# engine = create_engine('sqlite://', echo=False)
+	# for file in file_name:
+	pd_file = root_folder + file_name + file_type
+	df = pd.read_csv(pd_file, compression='gzip', header = 0, sep='\t', comment='#')
+		# df.to_sql(file_name, con=engine)
+	return df 
+
+file_name = ["oma-uniprot", "oma-ncbi"]
 file_type = ".txt.gz"
 root_folder = './cache/'
 
-def download_data(file_name, file_type, root_folder):
-	print('Beginning file download with urllib...')
-	BASE_URL = "https://omabrowser.org/All/"
-	#read in gzip files
-	for file in file_name:
-		download_url = BASE_URL + file + file_type
-		storage_location = root_folder + file + file_type
-		response = urllib.request.urlretrieve(download_url, storage_location)
+uniprot = download_data(file_name[0],file_type,root_folder)
+oma_gi = download_data(file_name[1],file_type,root_folder)
 
-def load_data(file_name, file_tpye, root_folder):
-	for file in file_name:
-		pd_file = root_folder + file + file_type
-		df = pd.read_csv(pd_file, compression='gzip', header = 0, sep='\t', comment='#')
-
+uniprot_df = load_data(file_name[0], file_type, root_folder)
+oma_ncbi_df = load_data(file_name[1], file_type, root_folder)
 
