3a81821253ac913826bdaaf1240c65529cacf9e9
lzy7071 <zhouyang.lian@familian.life>
2019-01-28 15:58:59 -0500

    completed uniprot_ncbi.py for mapping uniprot to ncbi

--- datanator/data_source/uniprot_ncbi.py
+++ datanator/data_source/uniprot_ncbi.py
@@ -10,41 +10,9 @@ import sqlalchemy
 from sqlalchemy import Column, Integer, Float, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 import urllib.request
 
 Base = sqlalchemy.ext.declarative.declarative_base()
-
-class Uniprot(Base):
-    """ Represents the oma-uniprot association table
-
-    Attributes:
-        oma (:obj:`str`): oma id
-        uniprot (:obj:`list`): uniprot id
-    """
-    _id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True)
-
-    oma = sqlalchemy.Column(sqlalchemy.String(), unique=True, index=True)
-
-    uniprot = sqlalchemy.Column(sqlalchemy.String(), unique=True, index=True)
-
-    __tablename__ = 'uniprot'
-
-class Ncbi(Base):
-    """ Represents the oma-ncbi association table
-
-    Attributes:
-        oma (:obj:`str`): oma id
-        ncbi (:obj:`list'): ncbi id
-    """
-    _id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True)
-
-    oma = sqlalchemy.Column(sqlalchemy.String(), unique=True, index=True)
-
-    ncbi = sqlalchemy.Column(sqlalchemy.String(), unique=True, index=True)
-
-    __tablename__ = 'ncbi'
-
 
 def download_data(file_name, file_type, root_folder):
 	print('Beginning file download with urllib: Downloading  ' + file_name)
@@ -62,17 +30,25 @@ def load_data(file_name, file_tpye, root_folder):
 	# engine = create_engine('sqlite://', echo=False)
 	# for file in file_name:
 	pd_file = root_folder + file_name + file_type
-	df = pd.read_csv(pd_file, compression='gzip', header = 0, sep='\t', comment='#')
+	df = pd.read_csv(pd_file, compression='gzip', header = None, names = [x.upper() for x in file_name.split('-')], sep='\t', comment='#')
 		# df.to_sql(file_name, con=engine)
 	return df 
 
 file_name = ["oma-uniprot", "oma-ncbi"]
 file_type = ".txt.gz"
-root_folder = './cache/'
+root_folder = 'cache/'
 
 uniprot = download_data(file_name[0],file_type,root_folder)
-oma_gi = download_data(file_name[1],file_type,root_folder)
+oma_ncbi = download_data(file_name[1],file_type,root_folder)
 
 uniprot_df = load_data(file_name[0], file_type, root_folder)
-oma_ncbi_df = load_data(file_name[1], file_type, root_folder)
+ncbi_df = load_data(file_name[1], file_type, root_folder)
 
+print('merging dataframes...')
+ncbi_uniprot = pd.merge(uniprot_df, ncbi_df, how='outer', on=file_name[0].split('-')[0].upper())
+
+# toStore = NcbiUniprot(oma=ncbi_uniprot[0], uniprot=ncbi_uniprot['1_x'], ncbi=ncbi_uniprot['1_y'])
+print('store merged dataframes in sql file...')
+engine = sqlalchemy.create_engine('sqlite:///' + root_folder + 'ncbi_uniprot.sqlite')
+# db_session = sqlalchemy.orm.sessionmaker(bind=engine)()
+ncbi_uniprot.to_sql('ncbi_uniprot', con=engine)
