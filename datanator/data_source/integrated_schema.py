09c8f7b5fcdebf6e4a537c650a125cf3a3d3540d
lzy7071 <zhouyang.lian@familian.life>
2019-03-01 16:16:11 -0500

    integrated_schema 20190301

--- datanator/data_source/integrated_schema.py
+++ datanator/data_source/integrated_schema.py
@@ -65,13 +65,13 @@ reaction_compound = sqlalchemy.Table(
 
 class Synonym(Base):
 
-	_id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True)
+	_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
 	label = sqlalchemy.Column(sqlalchemy.String(), index=True)
 	__tablename__ = 'synonym'
 
 class Entry(Base):
 
-	_id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True)
+	_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
 	name = sqlalchemy.Column(sqlalchemy.String())
 	synonyms = sqlalchemy.orm.relationship('Synonym', secondary=entry_synonym, backref=sqlalchemy.orm.backref('entries'))
 	comments = sqlalchemy.Column(sqlalchemy.String())
@@ -85,7 +85,7 @@ class Entry(Base):
 
 class Compartment(Entry):
 
-	_id = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey('entry._id'), primary_key=True)
+	_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('entry._id'), primary_key=True)
 	name = sqlalchemy.Column(sqlalchemy.String())
 
 	__tablename__ = 'compartment'
@@ -93,12 +93,12 @@ class Compartment(Entry):
 
 class Compound(Entry):
 
-	_id = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey('entry._id'), primary_key=True)
+	_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('entry._id'), primary_key=True)
 	structures = sqlalchemy.orm.relationship('BioPolymerForm', secondary=compound_compound_structure,
                                              backref=sqlalchemy.orm.backref('compounds'))
 	_emprical_formula = sqlalchemy.Column(sqlalchemy.String())
 	_mol_wt = sqlalchemy.Column(sqlalchemy.Float())
-	_charge = sqlalchemy.Column(sqlalchemy.Integer())
+	_charge = sqlalchemy.Column(sqlalchemy.Integer)
 
 	__tablename__ = 'compound'
 	__mapper_args__ = {'polymorphic_identity': 'compound'}
@@ -116,7 +116,7 @@ class Compound(Entry):
 		return [s.value for s in self.structures if s.format == 'smiles']
 
 class CompoundStructure(Base):
-	_id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True)
+	_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
 	value = sqlalchemy.Column(sqlalchemy.Text())
 	_format = sqlalchemy.Column(sqlalchemy.String())
 	_value_inchi = sqlalchemy.Column(sqlalchemy.Text(), index=True)
@@ -171,10 +171,10 @@ class Protein(Compound):
 
 
 class InteractionParticipant(Entry):
-	_id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True)
-	compound_id = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey('compound._id'), index=True)
+	_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+	compound_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('compound._id'), index=True)
 	compound = sqlalchemy.orm.relationship('Compound', backref=sqlalchemy.orm.backref('interaction_participants'), foreign_keys=[compound_id])
-	compartment_id = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey('compartment._id'), index=True)
+	compartment_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('compartment._id'), index=True)
 	compartment = sqlalchemy.orm.relationship('Compartment', backref=sqlalchemy.orm.backref('interaction_participants'), foreign_keys=[compartment_id])
 	coefficient = sqlalchemy.Column(sqlalchemy.Float())
 	type = sqlalchemy.Column(sqlalchemy.String())
@@ -185,7 +185,7 @@ class InteractionParticipant(Entry):
 
 class Complex(Compound):
 	
-	_id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True)
+	_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
 	synonyms = sqlalchemy.orm.relationship('InteractionParticipant', secondary=complex_interaction_participant, backref=sqlalchemy.orm.backref('complexes'))
 
 	__tablename__ = 'complex'
@@ -194,13 +194,13 @@ class Enzyme(Entry):
 	"""
 	represents enzymes 
 	"""
-	_id = sqlalchemy.Column(sqlalchemy.Integer(), sqlalchemy.ForeignKey('entry._id'), primary_key=True)
+	_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('entry._id'), primary_key=True)
 	molecular_weight = sqlalchemy.Column(sqlalchemy.Float())
 	__tablename__ = 'enzyme'
 	__mapper_args__ = {'polymorphic_identity': 'enzyme'}
 
 class Reaction(Entry):
-	_id = sqlalchemy.Column(sqlalchemy.Integer(),sqlalchemy.ForeignKey('entry._id'), primary_key=True)
+	_id = sqlalchemy.Column(sqlalchemy.Integer,sqlalchemy.ForeignKey('entry._id'), primary_key=True)
 	participants = sqlalchemy.orm.relationship('InteractionParticipant', secondary=reaction_interaction_participant, backref=sqlalchemy.orm.backref('reaction_participants'))
 	#enzymes: things that affect the rate law but don't undergo permanent covalent transformations
 	modifiers = sqlalchemy.orm.relationship('Compound', secondary=reaction_compound, backref=sqlalchemy.orm.backref('reaction_modifiers'))
@@ -219,7 +219,7 @@ class Observation(Base):
 	value = sqlalchemy.orm.relationship('BioPolymerForm', secondary=compound_compound_structure,
                                              backref=sqlalchemy.orm.backref('compounds'))
 	# biological system observed 
-	taxon = sqlalchemy.Column(sqlalchemy.Integer(), nullable = True)
+	taxon = sqlalchemy.Column(sqlalchemy.Integer, nullable = True)
 	wildtype = sqlalchemy.Column(sqlalchemy.Boolean(), nullable = True)
 	genetic_variant = sqlalchemy.Column(sqlalchemy.String(), nullable = True)
 	tissue = sqlalchemy.Column(sqlalchemy.String())
@@ -254,7 +254,7 @@ class Observation(Base):
 	retrieval_method = sqlalchemy.Column(sqlalchemy.String()) # scripts used in data_source to retrieve the data
 	retrieval_date = sqlalchemy.Column(sqlalchemy.DateTime,default=datetime.datetime.utcnow())
 
-	pubs = ...
+	pubs = sqlalchemy.orm.relationship('Resource', secondary=resource_publication, backref=sqlalchemy.orm.backref('publications'))
 
 	__tablename__='observation'
 	__mapper_args__ = {'polymorphic_on': type}
@@ -265,8 +265,6 @@ class QuantitativeObservation(Observation):
 	error = sqlalchemy.Column(Float())
 	units = sqlalchemy.Column(String())
 
-
-
 class QualitativeObservation(Observation):
 	value = sqlalchemy.Column(String())
 
@@ -276,9 +274,15 @@ class Resource(Base):
 	sqlalchemy.schema.UniqueConstraint(namespace, _id)
 	__tablename__ = 'resource'
 
-external_reference_publication = sqlalchemy.Table(
-    'external_reference_publication', Base.metadata,
-    sqlalchemy.Column('external_reference__id', sqlalchemy.Integer, sqlalchemy.ForeignKey('external_references._id'), index=True),
+observation_publication = sqlalchemy.Table(
+	'observation_publication', Base.metadata,
+	sqlalchemy.Column('observation__id', sqlalchemy.Integer, sqlalchemy.ForeignKey('resource._id'), index=True),
+	sqlalchemy.Column('publication__id',sqlalchemy.Integer, sqlalchemy.ForeignKey('publication._id'), index=True),
+)
+
+resource_publication = sqlalchemy.Table(
+    'resource_publication', Base.metadata,
+    sqlalchemy.Column('resource__id', sqlalchemy.Integer, sqlalchemy.ForeignKey('resources._id'), index=True),
     sqlalchemy.Column('publication__id', sqlalchemy.Integer, sqlalchemy.ForeignKey('publications._id'), index=True),
 )
 
@@ -297,8 +301,8 @@ class Publication(Base):
 	edition = sqlalchemy.Column(sqlalchemy.Integer())
 	chapter = sqlalchemy.Column(sqlalchemy.Integer())
 	pages = sqlalchemy.Column(sqlalchemy.String())
-	db_refs = sqlalchemy.orm.relationship("ExternalReference", secondary = external_references_publication, backref='publications')
-	__tablename__ = 'publications'
+	db_refs = sqlalchemy.orm.relationship("Resource", secondary = resources_publication, backref='publication')
+	__tablename__ = 'publication'
 
 
 class SabirrayEC(data_source.HttpDataSource):
