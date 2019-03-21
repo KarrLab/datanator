d4bba784c298ae2ed1ca466d3985948d0d5f5c56
lzy7071 <zhouyang.lian@familian.life>
2019-03-21 16:28:20 -0400

    sqlite_to_json.py parses sqlite db into json files

---
+++ tests/data_source/test_sqlite_to_json.py
@@ -0,0 +1,74 @@
+'''Tests of sqlite_to_json
+'''
+
+import unittest
+import shutil
+import tempfile
+from datanator.data_source import sqlite_to_json
+import json
+
+
+class TestSQLToJSON(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.cache_dirname = tempfile.mkdtemp()
+        cls.database = '../../datanator/data_source/cache/SabioRk.sqlite'
+        cls.query = "select * from "
+        cls.src = sqlite_to_json.SQLToJSON(cls.database, cls.query)
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls.cache_dirname)
+
+    def test_table(self):
+        table_names = self.src.table()
+        self.assertEqual(table_names, ['synonym', 'resource', 'entry', 'compound_structure', 'entry_synonym', 'entry_resource', 'compound', 'enzyme',
+                                       'compartment', 'compound_compound_structure', 'kinetic_law', 'enzyme_subunit', 'kinetic_law_resource', 'reaction_participant', 'parameter'])
+
+    def test_query_table(self):
+        tables = ['kinetic_law', 'resource']
+        data = {}
+        for table in tables:
+            result = self.src.query_table(table)
+            data[table] = result
+        null = None
+        self.assertEqual(data['kinetic_law'][0],
+                         {"_id": 5,
+                          "enzyme_id": 3,
+                          "enzyme_compartment_id": null,
+                          "enzyme_type": "",
+                          "tissue": null,
+                          "mechanism": null,
+                          "equation": null,
+                          "taxon": 1467,
+                          "taxon_wildtype": 1,
+                          "taxon_variant": "variant DSAI (N76D/N87S/S103A/V104I)",
+                          "temperature": 25.0,
+                          "ph": 7.5,
+                          "media": "50 mM potassium phosphate, 4 % DMSO"}
+                         )
+        self.assertEqual(data['kinetic_law'][1],
+                         {
+            "_id": 11,
+            "enzyme_id": 10,
+            "enzyme_compartment_id": null,
+            "enzyme_type": "",
+            "tissue": null,
+            "mechanism": null,
+            "equation": null,
+            "taxon": 1467,
+            "taxon_wildtype": 0,
+            "taxon_variant": "S156E of subtilisin DSAI (N76D/N87S/S103A/V104I)",
+            "temperature": 25.0,
+            "ph": 7.5,
+            "media": "50 mM potassium phosphate, 4 % DMSO"
+        }
+        )
+        self.assertEqual(data['resource'][0],
+                         {
+            "_id": 1,
+            "namespace": "chebi",
+            "id": "CHEBI:16670"
+        }
+        )
