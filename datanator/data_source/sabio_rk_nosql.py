a8a274627da8dfa938f7871527bdf586415daafe
Zhouyang Lian <lzy7071@gmail.com>
2019-03-23 21:37:27 -0400

    sabio_rk_nosql.py PEP8 formatting

--- datanator/data_source/sabio_rk_nosql.py
+++ datanator/data_source/sabio_rk_nosql.py
@@ -1,5 +1,5 @@
 '''Parse SabioRk json files into MongoDB documents
-	(json files acquired by running sqlite_to_json.py)
+    (json files acquired by running sqlite_to_json.py)
 '''
 
 import json
@@ -49,50 +49,50 @@ class SabioRkNoSQL():
         return (file_names, file_dict)
 
     '''loads dictionaries as documents (1 kinetic_law per document)
-    	Attributes:
-    		
-    		file_names: list of name of files
-    		file_dict: dictionaries of info for each file e.g.
-    					{'entry': {...}, 'kinetic_law': {...},  }
+        Attributes:
+            
+            file_names: list of name of files
+            file_dict: dictionaries of info for each file e.g.
+                        {'entry': {...}, 'kinetic_law': {...},  }
     '''
+
     def make_doc(self, file_names, file_dict):
-    	'''
-    	Start iteration with kinetic_law
+        '''
+        Start iteration with kinetic_law
 
         '''
         for i in range(len(file_names)):
-        	vars()[file_names[i] + '_dict'] = file_dict[file_names[i]]
+            vars()[file_names[i] + '_dict'] = file_dict[file_names[i]]
 
         for i in range(len(kinetic_law_dict)):
-        	json_name = 'kinlaw_id_' + str(i)
-        	cur_kinlaw_dict = kinetic_law_dict[i]
-        	sabio_doc = {}
-        	sabio_doc['kinlaw_id'] = i
-        	for key in cur_dict:
-        		if key != '_id':
-        			vars()[key] = cur_kinlaw_dict[key]
-        		else:
-        			kinlaw_entry_id = cur_dict[key]
-        	
-        	sabio_doc['compartment'] = []
-        	sabio_doc['compound'] = []
-        	sabio_doc['compound_structure'] = []
-        	sabio_doc['resource'] = []
-        	sabio_doc['enzyme'] = []
-        	sabio_doc['synonym'] = []
-        	sabio_doc['enzyme_subunit'] = []
-        	sabio_doc['kinetic_law'] = []
-        	sabio_doc['parameter'] = []
-        	sabio_doc['reaction_participant']
+            json_name = 'kinlaw_id_' + str(i)
+            cur_kinlaw_dict = kinetic_law_dict[i]
+            sabio_doc = {}
+            sabio_doc['kinlaw_id'] = i
+            for key in cur_dict:
+                if key != '_id':
+                    vars()[key] = cur_kinlaw_dict[key]
+                else:
+                    kinlaw_entry_id = cur_dict[key]
 
-        	cur_enzyme_dict = next(item for item in enzyme_dict if enzyme_dict['_id'] == enzyme_id)
-        	enzyme_mol_weight = cur_enzyme_dict['molecular_weight'] 
-        	enzyme_id = entry_dict[enzyme_id - 1]['id']
-        	enzyme_name = entry_dict[enzyme_id - 1]['name']
-        	enzyme_created = entry_dict[enzyme_id - 1]['created']
-        	enzyme_modified = entry_dict[enzyme_id - 1]['modified']
+            sabio_doc['compartment'] = []
+            sabio_doc['compound'] = []
+            sabio_doc['compound_structure'] = []
+            sabio_doc['resource'] = []
+            sabio_doc['enzyme'] = []
+            sabio_doc['synonym'] = []
+            sabio_doc['enzyme_subunit'] = []
+            sabio_doc['kinetic_law'] = []
+            sabio_doc['parameter'] = []
+            sabio_doc['reaction_participant']
 
-        	substrates = []
-        	products = []
+            cur_enzyme_dict = next(
+                item for item in enzyme_dict if enzyme_dict['_id'] == enzyme_id)
+            enzyme_mol_weight = cur_enzyme_dict['molecular_weight']
+            enzyme_id = entry_dict[enzyme_id - 1]['id']
+            enzyme_name = entry_dict[enzyme_id - 1]['name']
+            enzyme_created = entry_dict[enzyme_id - 1]['created']
+            enzyme_modified = entry_dict[enzyme_id - 1]['modified']
 
-
+            substrates = []
+            products = []
