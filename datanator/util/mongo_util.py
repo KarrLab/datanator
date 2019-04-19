7e698d9d094c380bc70e8067fbf9dfe592ff1c25
Zhouyang Lian <zhouyang.lian@familian.life>
2019-04-19 17:42:51 -0400

    building connecting and performance measurement unitilities for mongodb operations (mongo_util.py, test_mongo_util.py)

--- datanator/util/mongo_util.py
+++ datanator/util/mongo_util.py
@@ -8,7 +8,7 @@ class MongoUtil():
                  collection=None, verbose=False, max_entries=float('inf')):
         self.cache_dirname = cache_dirname
         self.MongoDB = MongoDB
-        self.db = self.db
+        self.db = db
         self.collection = collection
         self.verbose = verbose
         self.max_entries = max_entries
@@ -24,19 +24,20 @@ class MongoUtil():
         except pymongo.errors.ConnectionFailure:
             return ('Server not available')
 
-    def doc_feeder(collection, step=1000, s=None, e=None, inbatch=False, query=None, batch_callback=None, fields=None):
+    def doc_feeder(self,collection, step=1000, s=None, e=None, inbatch=False, query=None, batch_callback=None, projection=None):
         '''A iterator for returning docs in a collection, with batch query.
            additional filter query can be passed via "query", e.g.,
            doc_feeder(collection, query={'taxid': {'$in': [9606, 10090, 10116]}})
            batch_callback is a callback function as fn(cnt, t), called after every batch
            fields is optional parameter passed to find to restrict fields to return.
         '''
-        cur = collection.find(query, timeout=False, fields=fields)
+        cur = collection.find(query, no_cursor_timeout=False, projection=projection)
         n = cur.count()
         s = s or 0
         e = e or n
-        print('Retrieving %d documents from database "%s".' %
-              (n, collection.name))
+        if self.verbose:
+            print('Retrieving %d documents from collection "%s".' %
+                  (n, collection.name))
         t0 = time.time()
         if inbatch:
             doc_li = []
@@ -46,11 +47,13 @@ class MongoUtil():
             if s:
                 cur.skip(s)
                 cnt = s
-                print("Skipping %d documents." % s)
+                if self.verbose:
+                    print("Skipping %d documents." % s)
             if e:
                 cur.limit(e - (s or 0))
             cur.batch_size(step)
-            print("Processing %d-%d documents..." %
+            if self.verbose:
+                print("Processing %d-%d documents..." %
                   (cnt + 1, min(cnt + step, e)), end='')
             for doc in cur:
                 if inbatch:
@@ -62,25 +65,28 @@ class MongoUtil():
                     if inbatch:
                         yield doc_li
                         doc_li = []
-                    print('Done.[%.1f%%,%s]' % (cnt * 100. / n, timesofar(t1)))
+                    if self.verbose:
+                        print('Done.[%.1f%%,%s]' % (cnt * 100. / n, self.timesofar(t1)))
                     if batch_callback:
                         batch_callback(cnt, time.time()-t1)
                     if cnt < e:
                         t1 = time.time()
-                        print("Processing %d-%d documents..." %
-                              (cnt + 1, min(cnt + step, e)), end='')
+                        if self.verbose:
+                            print("Processing %d-%d documents..." %
+                                  (cnt + 1, min(cnt + step, e)), end='')
             if inbatch and doc_li:
                 # Important: need to yield the last batch here
                 yield doc_li
 
             # print 'Done.[%s]' % timesofar(t1)
-            print('Done.[%.1f%%,%s]' % (cnt * 100. / n, timesofar(t1)))
-            print("=" * 20)
-            print('Finished.[total time: %s]' % timesofar(t0))
+            if self.verbose:
+                print('Done.[%.1f%%,%s]' % (cnt * 100. / n, self.timesofar(t1)))
+                print("=" * 20)
+                print('Finished.[total time: %s]' % self.timesofar(t0))
         finally:
             cur.close()
 
-    def ask(prompt, options='YN'):
+    def ask(self, prompt, options='YN'):
         '''Prompt Yes or No,return the upper case 'Y' or 'N'.
         '''
         options = options.upper()
@@ -90,7 +96,7 @@ class MongoUtil():
                 break
         return s
 
-    def timesofar(t0, clock=0, t1=None):
+    def timesofar(self, t0, clock=0, t1=None):
         '''return the string(eg.'4m5.32s') for the passed real time/CPU time so far
            from given t0 (clock 0 for cpu time 1 for realtime).
         '''
---
+++ tests/util/test_mongo_util.py
@@ -0,0 +1,34 @@
+import unittest
+from datanator.util import mongo_util
+import tempfile
+import shutil
+
+
+class TestMongoUtil(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.cache_dirname = tempfile.mkdtemp()
+        cls.db = 'test'
+        cls.MongoDB = 'mongodb://mongo:27017/'
+        cls.collection = 'ecmdb'
+        cls.src = mongo_util.MongoUtil(
+            cls.cache_dirname, cls.MongoDB, cls.db, cls.collection, verbose=True, max_entries=20)
+        cls.client, cls.db, cls.collection_obj = cls.src.con_db()
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls.cache_dirname)
+        cls.client.close()
+
+    def test_con_db(self):
+        self.assertNotEqual(self.db, 'Server not available')
+
+    def test_doc_feeder(self):
+    	query = {'m2m_id': {'$in': ["M2MDB000004", "M2MDB000005", "M2MDB000006"]}}
+    	col = self.src.doc_feeder(self.collection_obj, query=query)
+    	for doc in col:
+    		if doc['m2m_id'] == "M2MDB000005":
+    			self.assertEqual(doc['accession'], "ECMDB00023")
+    		elif doc['accession'] == "ECMDB00019":
+    			self.assertEqual(doc['m2m_id'], "M2MDB000004")
