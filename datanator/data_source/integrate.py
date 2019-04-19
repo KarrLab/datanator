b9a014b7ab7f6fc74ce321fd91ab49707d700ad6
Zhouyang Lian <zhouyang.lian@familian.life>
2019-04-19 16:05:57 -0400

    find metabolite's kegg_id given a list

--- datanator/data_source/integrate.py
+++ datanator/data_source/integrate.py
@@ -30,25 +30,40 @@ class Integrate():
     def retrieve_sabio(self, collection, taxon):
         return collection.find({'taxon': taxon}).limit(self.max_entries)
 
-
-    '''Find reaction participant's chebi and return as two lists
+    '''Find reaction participant's kegg id and return as two lists
     	(substrate, product)
     '''
 
-    def find_sabio_chebi(self, cursor):
-            i = 0
-            substrate_chebi = []
-            product_chebi = []
-            for doc in cursor:
-                if self.verbose:
-                	print (i)
-                substrate_list = doc['reaction_participant'][0]['substrate']
-                substrate_chebi += [dic['kegg_id'] for dic in substrate_list]
-                product_list = doc['reaction_participant'][1]['product']
-                product_chebi += [dic['kegg_id'] for dic in product_list]
-                i += 1
+    def find_sabio_kegg(self, cursor):
+        i = 0
+        substrate_kegg = []
+        product_kegg = []
+        for doc in cursor:
+            if self.verbose:
+                print('Processing document {} of {}'.format(
+                    i+1, self.max_entries))
+            substrate_list = doc['reaction_participant'][0]['substrate']
+            substrate_kegg += [dic['kegg_id'] for dic in substrate_list]
+            product_list = doc['reaction_participant'][1]['product']
+            product_kegg += [dic['kegg_id'] for dic in product_list]
+            i += 1
 
-                if i > self.max_entries:
-                    break
+            if i > self.max_entries:
+                break
+        if self.verbose:
+            print(substrate_kegg, product_kegg)
+        return (substrate_kegg, product_kegg)
 
-            return (substrate_chebi, product_chebi)
+    '''Find metabolite documents with the specified kegg_id
+    	and return the cursor containing documents containing specified field
+    	Attributes:
+    		collection: colection to be queried (e.g. ECMDB, YMDB)
+    		kegg_id: list of kegg_id to be used for querying
+    		fields: dictionary fields of interest to be returned with the documents
+    '''
+
+    def find_metabolite_kegg(self, collection, kegg_id, fileds):
+        collection.create_index('kegg_id')
+        return collection.find({'kegg_id': {'$in': kegg_id}}, fileds).sort([('kegg_id', 1)])
+
+
--- tests/data_source/test_integrate.py
+++ tests/data_source/test_integrate.py
@@ -15,6 +15,8 @@ class TestIntegrate(unittest.TestCase):
         cls.src = integrate.Integrate(
             cls.cache_dirname, cls.MongoDB, cls.db, verbose=True, max_entries=20)
         cls.client, cls.db = cls.src.con_db()
+        cls.e_coli = 562  # E. Coli taxon id
+        cls.s_cere = 4932  # Taxon ID for Saccharomyces cerevisiae
 
     @classmethod
     def tearDownClass(cls):
@@ -28,16 +30,31 @@ class TestIntegrate(unittest.TestCase):
     @unittest.skip('passed')
     def test_retrieve_sabio(self):
         collection = self.db['sabio_rk']
-        taxon = 562  # E. Coli taxon id
-        cursor = self.src.retrieve_sabio(collection, taxon)
+        cursor = self.src.retrieve_sabio(collection, self.e_coli)
         self.assertEqual(cursor.count(with_limit_and_skip=True), 20)
         self.assertEqual(cursor[0]['kinlaw_id'], 47)
         self.assertEqual(cursor[0]['parameter'][0]['observed_value'], 0.074)
 
+    #@unittest.skip('passed')
     def test_find_chebi(self):
         collection = self.db['sabio_rk']
-        taxon = 562  # E. Coli taxon id
-        cursor = self.src.retrieve_sabio(collection, taxon)
-        substrate_chebi, product_chebi = self.src.find_sabio_chebi(cursor)
-        self.assertTrue('C00019' in substrate_chebi)
+        cursor = self.src.retrieve_sabio(collection, self.e_coli)
+        substrate_kegg, product_kegg = self.src.find_sabio_kegg(cursor)
+        self.assertTrue('C00019' in substrate_kegg)
+
+    #@unittest.skip('passed')
+    def test_find_metabolite_kegg(self):
+        collection = self.db['ecmdb']
+        substrate_kegg = ['C01092', 'C00019', 'C00019', 'C01092', 'C01092', 'C00019', 'C00019', 'C01092', 'C01137', 'C01092', 'C00019', 'C01092', 'C00019', 'C01092', 'C00001', 'C00012', 'C01092', 'C01137', 'C01092',
+                          'C00019', 'C00012', 'C00001', 'C00012', 'C00001', 'C00012', 'C00001', 'C00001', 'C00012', 'C00001', 'C00012', 'C00001', 'C00012', 'C00001', 'C00012', 'C00001', 'C00012', 'C00001', 'C00012', 'C00012', 'C00001']
+        product_kegg = ['C04425', 'C01037', 'C04425', 'C01037', 'C04425', 'C01037', 'C01037', 'C04425', None, 'C01037', 'C04425', 'C01037', 'C04425', 'C01037', 'C00012', 'C00012', None, 'C01037', 'C04425', 'C01037',
+                        'C00012', 'C00012', 'C00012', 'C00012', 'C00012', 'C00012', 'C00012', 'C00012', 'C00012', 'C00012', 'C00012', 'C00012', 'C00012', 'C00012', 'C00012', 'C00012', 'C00012', 'C00012', 'C00012', 'C00012']
+        self.assertEqual(len(substrate_kegg), len(product_kegg))
+        fileds = {'accession': 1, 'm2m_id': 1, 'name': 1, 'synonyms': 1, '_id': 0}
+        cursor = self.src.find_metabolite_kegg(collection, substrate_kegg, fileds)
+        self.assertEqual(cursor[0]['m2m_id'], 'M2MDB000142')
+        self.assertEqual(cursor[0].get('description'), None)
+        self.assertEqual(cursor[1]['m2m_id'], 'M2MDB000289')
+        self.assertEqual(cursor[1]['synonyms']['synonym'][4], "5'-Deoxyadenosine-5'-L-methionine disulfate ditosylate")
+
 
