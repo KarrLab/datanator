ba0d0ae253ad07e22256c3b8cf15dd756a102d4f
Zhouyang Lian <zhouyang.lian@familian.life>
2019-04-25 12:24:55 -0400

    query_nosql.py in core and its corresponding test component

---
+++ datanator/core/query_nosql.py
@@ -0,0 +1,128 @@
+from datanator.util import mongo_util
+import time
+
+class DataQuery(mongo_util.MongoUtil):
+
+    def __init__(self, cache_dirname=None, MongoDB=None, db=None,
+                verbose=False, max_entries=float('inf')):
+
+        super(DataQuery, self).__init__(cache_dirname=cache_dirname, MongoDB=MongoDB, db=db,
+                    verbose=verbose, max_entries=max_entries)
+
+    def doc_feeder(self,collection_str, sym_link = False, step=1000, s=None, e=None, inbatch=False, query=None, batch_callback=None, projection=None):
+        '''A iterator for returning docs in a collection, with batch query.
+           additional filter query can be passed via "query", e.g.,
+           doc_feeder(collection_str, query={'taxid': {'$in': [9606, 10090, 10116]}})
+           batch_callback is a callback function as fn(cnt, t), called after every batch
+           fields is optional parameter passed to find to restrict fields to return.
+        '''
+        collection = self.fill_db(collection_str, sym_link)
+        cur = collection.find(query, no_cursor_timeout=False, projection=projection)
+        n = cur.count()
+        s = s or 0
+        e = e or n
+        if self.verbose:
+            print('Retrieving %d documents from collection "%s".' %
+                  (n, collection.name))
+        t0 = time.time()
+        if inbatch:
+            doc_li = []
+        cnt = 0
+        t1 = time.time()
+        try:
+            if s:
+                cur.skip(s)
+                cnt = s
+                if self.verbose:
+                    print("Skipping %d documents." % s)
+            if e:
+                cur.limit(e - (s or 0))
+            cur.batch_size(step)
+            if self.verbose:
+                print("Processing %d-%d documents..." %
+                  (cnt + 1, min(cnt + step, e)), end='')
+            for doc in cur:
+                if inbatch:
+                    doc_li.append(doc)
+                else:
+                    yield doc
+                cnt += 1
+                if cnt % step == 0:
+                    if inbatch:
+                        yield doc_li
+                        doc_li = []
+                    if self.verbose:
+                        print('Done.[%.1f%%,%s]' % (cnt * 100. / n, self.timesofar(t1)))
+                    if batch_callback:
+                        batch_callback(cnt, time.time()-t1)
+                    if cnt < e:
+                        t1 = time.time()
+                        if self.verbose:
+                            print("Processing %d-%d documents..." %
+                                  (cnt + 1, min(cnt + step, e)), end='')
+            if inbatch and doc_li:
+                # Important: need to yield the last batch here
+                yield doc_li
+
+            # print 'Done.[%s]' % timesofar(t1)
+            if self.verbose:
+                print('Done.[%.1f%%,%s]' % (cnt * 100. / n, self.timesofar(t1)))
+                print("=" * 20)
+                print('Finished.[total time: %s]' % self.timesofar(t0))
+        finally:
+            cur.close()
+
+    def ask(self, prompt, options='YN'):
+        '''Prompt Yes or No,return the upper case 'Y' or 'N'.
+        '''
+        options = options.upper()
+        while 1:
+            s = input(prompt+'[%s]' % '|'.join(list(options))).strip().upper()
+            if s in options:
+                break
+        return s
+
+    def timesofar(self, t0, clock=0, t1=None):
+        '''return the string(eg.'4m5.32s') for the passed real time/CPU time so far
+           from given t0 (clock 0 for cpu time 1 for realtime).
+        '''
+        t1 = t1 or time.clock() if clock else time.time()
+        t = t1 - t0
+        h = int(t / 3600)
+        m = int((t % 3600) / 60)
+        s = round((t % 3600) % 60, 2)
+        t_str = ''
+        if h != 0:
+            t_str += '%sh' % h
+        if m != 0:
+            t_str += '%sm' % m
+        t_str += '%ss' % s
+        return t_str
+
+
+    def ask(self, prompt, options='YN'):
+        '''Prompt Yes or No,return the upper case 'Y' or 'N'.
+        '''
+        options = options.upper()
+        while 1:
+            s = input(prompt+'[%s]' % '|'.join(list(options))).strip().upper()
+            if s in options:
+                break
+        return s
+
+    def timesofar(self, t0, clock=0, t1=None):
+        '''return the string(eg.'4m5.32s') for the passed real time/CPU time so far
+           from given t0 (clock 0 for cpu time 1 for realtime).
+        '''
+        t1 = t1 or time.clock() if clock else time.time()
+        t = t1 - t0
+        h = int(t / 3600)
+        m = int((t % 3600) / 60)
+        s = round((t % 3600) % 60, 2)
+        t_str = ''
+        if h != 0:
+            t_str += '%sh' % h
+        if m != 0:
+            t_str += '%sm' % m
+        t_str += '%ss' % s
+        return t_str
--- tests/core/test_query_nosql.py
+++ tests/core/test_query_nosql.py
@@ -1,17 +1,17 @@
 import unittest
-from datanator.util import mongo_util
+from datanator.core import query_nosql
 import tempfile
 import shutil
 
 
-class TestMongoUtil(unittest.TestCase):
+class TestQueryNoSQL(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         cls.cache_dirname = tempfile.mkdtemp()
         cls.db = 'test'
         cls.MongoDB = 'mongodb://mongo_secondary:27017/'
-        cls.src = mongo_util.MongoUtil(
+        cls.src = query_nosql.DataQuery(
             cls.cache_dirname, cls.MongoDB, cls.db, verbose=True, max_entries=20)
         cls.collection_str = 'ecmdb'
         cls.client, cls.db, cls.collection_obj = cls.src.con_db(
@@ -23,11 +23,12 @@ class TestMongoUtil(unittest.TestCase):
         cls.client.drop_database(cls.db)
         cls.client.close()
 
-    def test_con_db(self):
-        self.assertNotEqual(self.db, 'Server not available')
-
-    def test_fill_db(self):
-        self.collection_obj.drop()
-        self.assertEqual(self.collection_obj.find().count(), 0)
-        collection_obj = self.src.fill_db(self.collection_str)
-        self.assertNotEqual(collection_obj.find().count(), 0)
+    def test_doc_feeder(self):
+        query = {'m2m_id': {
+            '$in': ["M2MDB000004", "M2MDB000005", "M2MDB000006"]}}
+        col = self.src.doc_feeder(self.collection_str, query=query)
+        for doc in col:
+            if doc['m2m_id'] == "M2MDB000005":
+                self.assertEqual(doc['accession'], "ECMDB00023")
+            elif doc['accession'] == "ECMDB00019":
+                self.assertEqual(doc['m2m_id'], "M2MDB000004")
