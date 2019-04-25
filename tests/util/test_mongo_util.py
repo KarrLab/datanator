3ca68eb27a71ec5e2f01981465d2a8de2c929eaa
Zhouyang Lian <zhouyang.lian@familian.life>
2019-04-25 15:20:07 -0400

    switched replicaset to none for circleci testing

--- tests/util/test_mongo_util.py
+++ tests/util/test_mongo_util.py
@@ -12,7 +12,7 @@ class TestMongoUtil(unittest.TestCase):
         cls.db = 'test'
         cls.MongoDB = 'mongodb://mongo:27017/'
         cls.src = mongo_util.MongoUtil(
-            cls.cache_dirname, cls.MongoDB, 'rs0', cls.db, verbose=True, max_entries=20)
+            cls.cache_dirname, cls.MongoDB, None, cls.db, verbose=True, max_entries=20)
         cls.collection_str = 'ecmdb'
         cls.client, cls.db, cls.collection_obj = cls.src.con_db(
             cls.collection_str)
