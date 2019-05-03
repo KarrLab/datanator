d928cb7f6ac707c62c1729fc345d64c7974a2124
Zhouyang Lian <zhouyang.lian@familian.life>
2019-05-03 12:14:08 -0400

    testing in query_nosql for h1_hesc query functions

--- tests/core/test_query_nosql.py
+++ tests/core/test_query_nosql.py
@@ -12,7 +12,7 @@ class TestQueryNoSQL(unittest.TestCase):
         cls.db = 'test'
         cls.MongoDB = 'mongodb://mongo:27017/'
         cls.src = query_nosql.DataQuery(
-            cls.cache_dirname, cls.MongoDB, None, cls.db, verbose=True, max_entries=20)
+            cls.cache_dirname, cls.MongoDB, 'rs0', cls.db, verbose=True, max_entries=20)
         cls.collection_str = 'ecmdb'
         cls.client, cls.db, cls.collection_obj = cls.src.con_db(
             cls.collection_str)
@@ -32,3 +32,21 @@ class TestQueryNoSQL(unittest.TestCase):
                 self.assertEqual(doc['accession'], "ECMDB00023")
             elif doc['accession'] == "ECMDB00019":
                 self.assertEqual(doc['m2m_id'], "M2MDB000004")
+
+        taxon_range = list(range(100, 20000))
+        null = None
+        query = {'taxon': {'$in': taxon_range}, 
+                 'taxon_wildtype': {'$eq': 1}, 
+                 'temperature': {'$lte':45, '$gte': 20},
+                 'ph': {'$lte': 8, '$gte': 6},
+                 'parameter.value': {'$ne': null },
+                 'parameter.sbo_type': {'$in': [25,27] } }
+
+        # only return these fileds in projection
+        projection = {'kinlaw_id':1, 'resource': 1, 'enzymes.enzyme': 1, 
+                    'enzymes.subunit': 1, 'parameter': 1}
+
+        collection = self.src.doc_feeder('sabio_rk', query = query, projection=projection)
+        with self.assertRaises(KeyError):
+            next(collection)['reaction_participant']
+
