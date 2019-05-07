b9f892674f7be82c0697594c09e30308894cab82
Zhouyang Lian <zhouyang.lian@familian.life>
2019-05-07 13:35:30 -0400

    indexing mongoDB collections

--- tests/util/test_mongo_util.py
+++ tests/util/test_mongo_util.py
@@ -12,7 +12,7 @@ class TestMongoUtil(unittest.TestCase):
         cls.db = 'test'
         cls.MongoDB = 'mongodb://mongo:27017/'
         cls.src = mongo_util.MongoUtil(
-            cls.cache_dirname, cls.MongoDB, 'rs0', cls.db, verbose=True, max_entries=20)
+            cls.cache_dirname, cls.MongoDB, None, cls.db, verbose=True, max_entries=20)
         cls.collection_str = 'ecmdb'
         cls.client, cls.db, cls.collection_obj = cls.src.con_db(
             cls.collection_str)
@@ -23,17 +23,14 @@ class TestMongoUtil(unittest.TestCase):
         # cls.client.drop_database(cls.db)
         cls.client.close()
 
-    @unittest.skip('passed')
+    # @unittest.skip('passed')
     def test_con_db(self):
         self.assertNotEqual(self.db, 'Server not available')
 
-    @unittest.skip('passed')
+    # @unittest.skip('passed')
     def test_fill_db(self):
         self.collection_obj.drop()
         self.assertEqual(self.collection_obj.find().count(), 0)
         collection_obj = self.src.fill_db(self.collection_str)
         self.assertNotEqual(collection_obj.find().count(), 0)
-
-    # def test_index_corum(self):
-    #     self.src.index_corum('corum')
 
