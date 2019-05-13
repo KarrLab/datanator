a4955bfe8c2052158370fb4e7da500c302287b96
Zhouyang Lian <zhouyang.lian@familian.life>
2019-05-13 17:36:05 -0400

    kegg_orthology.py and its test script

--- datanator/data_source/kegg_orthology.py
+++ datanator/data_source/kegg_orthology.py
@@ -1,12 +1,14 @@
 import json
 import requests
+import os
 from datanator.util import mongo_util
-
 
 class KeggOrthology(mongo_util.MongoUtil):
 
     def __init__(self, cache_dirname, MongoDB, db, replicaSet=None, verbose=False, max_entries=float('inf')):
-
+            self.ENDPOINT_DOMAINS = {
+                'root': 'https://www.genome.jp/kegg-bin/download_htext?htext=ko00001&format=json&filedir=',
+            }
             self.cache_dirname = cache_dirname
             self.MongoDB = MongoDB
             self.db = db
@@ -16,29 +18,75 @@ class KeggOrthology(mongo_util.MongoUtil):
             super(KeggOrthology, self).__init__(cache_dirname=cache_dirname, MongoDB=MongoDB, replicaSet=replicaSet, db=db,
                                              verbose=verbose, max_entries=max_entries)
 
-    def get_json_ends(self, tree):
-        ends = []
-        nodes = [tree]
-        while nodes:
-            new_nodes = []
-            for node in nodes:
-                if 'children' in node:
-                    new_nodes = new_nodes + node['children']
-                else:
-                    ends.append(node['name'])
-            nodes = new_nodes
-        return(ends)
-
     def load_content(self):
-        data = json.load(open('ko00001.json'))
-        ends = get_json_ends(data)
-        ends = [line.split(" ")[0] for line in ends]
-        ends = ends[1:]
-        os.makedirs(os.path.join(
-            self.cache_dirname, self.collection), exist_ok=True)
-        for ko in ends:
-            info = requests.get("http://rest.kegg.jp/get/ko:{}".format(ko))
-            response.raise_for_status()
+        root_url = self.ENDPOINT_DOMAINS['root']
+        if self.verbose:
+            print('\n Downloading root kegg orthology file ...')
+        manager = requests.get(root_url)
+        manager.raise_for_status()
+        path = os.path.join(self.cache_dirname, self.collection)
+        os.makedirs(path, exist_ok=True)
+        file_name = manager.json()['name']
+        store_path = os.path.join(path, file_name)
+        data = manager.json()
+        with open(store_path, 'w') as f:
+            json.dump(data, f, indent=4)
 
-    def parse_ko_txt(self):
-        pass
+        names = self.extract_values(data, 'name')
+        names = [name for name in names if name[0]=='K']
+        self.download_ko(names)
+
+    def parse_ko_txt(self, path):
+        for filename in os.listdir(path):
+            if filename.endswith('.txt'):
+                collection = []
+                with open(filename, 'r') as f:
+                    doc = {}
+                    for i, line in enumerate(f):
+                        if i == 0: # ENTRY
+                            doc['entry'] = line.split()[1]
+                            doc['type'] = line.split()[2]
+                        elif i ==1: # NAME
+                            doc['gene_name'] = [item.replace(',','') for item in line.split()[1:]]
+                        elif i == 2: # DEFINITION
+                            doc['definition'] = line.split(' ',1)[1]
+                        elif line[:5] == 'GENES':
+                            continue
+
+        
+
+    def download_ko(self, names):
+        path = os.path.join(self.cache_dirname, self.collection)
+        os.makedirs(path, exist_ok=True)
+        for i, name in enumerate(names):
+            if i > self.max_entries:
+                break
+            if self.verbose and i%10 ==0:
+                print('Downloading {} of {} kegg orthology file ...'.format(i, min(len(names), self.max_tries)))
+            info = requests.get("http://rest.kegg.jp/get/ko:{}".format(name))
+            info.raise_for_status()
+            file_name = os.path.join(path, name)
+            with open(file_name, 'w') as f:
+                f.write(info.text)
+
+
+    def extract_values(self, obj, key):
+        """Pull all values of specified key from nested JSON."""
+        arr = []
+
+        def extract(obj, arr, key):
+            """Recursively search for values of key in JSON tree."""
+            if isinstance(obj, dict):
+                for k, v in obj.items():
+                    if isinstance(v, (dict, list)):
+                        extract(v, arr, key)
+                    elif k == key:
+                        arr.append(v)
+            elif isinstance(obj, list):
+                for item in obj:
+                    extract(item, arr, key)
+            return arr
+
+        results = extract(obj, arr, key)
+
+        return results
--- tests/data_source/test_kegg_orthology.py
+++ tests/data_source/test_kegg_orthology.py
@@ -3,36 +3,78 @@ from datanator.data_source import kegg_orthology
 import tempfile
 import shutil
 import pymongo
-
+import json
 
 class TestKeggOrthology(unittest.TestCase):
 
-    def setUp(self):
-        self.cache_dirname = tempfile.mkdtemp()
-        self.db = 'test'
-        self.MongoDB = 'mongodb://mongo:27017/'
+    @classmethod
+    def setUpClass(cls):
+        cls.cache_dirname = tempfile.mkdtemp()
+        cls.db = 'test'
+        cls.MongoDB = 'mongodb://mongo:27017/'
+        cls.collection_str = 'kegg_orthology'
+        cls.src = kegg_orthology.KeggOrthology(
+            cls.cache_dirname, cls.MongoDB, cls.db, replicaSet=None, verbose=True, max_entries=20)
+        cls.client, cls.db, cls.collection = cls.src.con_db(cls.collection_str)
+        cls.data = {
+            "name":"ko00001",
+            "children":[
+            {
+                "name":"09100 Metabolism",
+                "children":[
+                {
+                    "name":"09101 Carbohydrate metabolism",
+                    "children":[
+                    {
+                        "name":"00010 Glycolysis \/ Gluconeogenesis [PATH:ko00010]",
+                        "children":[
+                        {
+                            "name":"K00844  HK; hexokinase [EC:2.7.1.1]"
+                        },
+                        {
+                            "name":"K12407  GCK; glucokinase [EC:2.7.1.2]"
+                        },
+                        {
+                            "name":"K00845  glk; glucokinase [EC:2.7.1.2]"
+                        },
+                        {
+                            "name":"K01810  GPI, pgi; glucose-6-phosphate isomerase [EC:5.3.1.9]"
+                        }
+                        ]
+                    }]
+                }],
+                "name":"00020 Citrate cycle (TCA cycle) [PATH:ko00020]",
+                "children":[
+                {
+                    "name":"K01647  CS, gltA; citrate synthase [EC:2.3.3.1]"
+                },
+                {
+                    "name":"K01648  ACLY; ATP citrate (pro-S)-lyase [EC:2.3.3.8]"
+                },
+                {
+                    "name":"K15230  aclA; ATP-citrate lyase alpha-subunit [EC:2.3.3.8]"
+                },
+                {
+                    "name":"K15231  aclB; ATP-citrate lyase beta-subunit [EC:2.3.3.8]"
+                }
+                ]
+            }]}
 
-    def tearDown(self):
-        shutil.rmtree(self.cache_dirname)
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls.cache_dirname)
+        cls.client.close()
 
     def test_con_db(self):
-        src = kegg_orthology.KeggOrthology(
-            self.cache_dirname, self.MongoDB, self.db, replicaSet=None, verbose=True, max_entries=20)
-        client, db, collection = src.con_db('kegg_orthology')
-        self.assertNotEqual(collection, 'Server not available')
-        client.close()
+        self.assertNotEqual(self.collection, 'Server not available')
 
-    def test_get_json_ends(self):
-        pass
-
+    def test_extract_values(self):
+        data = json.dumps(self.data)
+        loaded_data = json.loads(data)
+        name_list = self.src.extract_values(loaded_data, 'name')
+        self.assertEqual(name_list[0], 'ko00001')
+        self.assertEqual(name_list[-1][:6], 'K15231')
+    
     def test_parse_ko_txt(self):
-        src = kegg_orthology.KeggOrthology(
-            self.cache_dirname, self.MongoDB, self.db, replicaSet=None, verbose=True, max_entries=20)
-        client, db, collection = src.con_db('kegg_orthology')
-
-
-
-        client.close()
-
-
+        pass
 
