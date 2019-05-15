67af16720ee235556aa83bdc08b72beedd5917f6
Zhouyang Lian <zhouyang.lian@familian.life>
2019-05-15 16:06:45 -0400

    bash scripts for cron jobs

--- scripts/mongodump.sh
+++ scripts/mongodump.sh
@@ -6,12 +6,9 @@ APP_NAME="datanator"
 MONGO_HOST="rs0/mongo:27017,mongo_secondary:27017,mongo_tertiary:27017"
 TIMESTAMP=`date +%F-%H%M`
 MONGODUMP_PATH="/usr/bin/mongodump"
-BACKUPS_DIR="/tmp"
-BACKUP_NAME="$APP_NAME-$TIMESTAMP"
- 
-$MONGODUMP_PATH -d $MONGO_DATABASE --host $MONGO_HOST 
- 
+BACKUPS_DIR="/root/host/karr_lab/datanator/datanator/data_source/cache"
 mkdir -p $BACKUPS_DIR
-mv dump $BACKUP_NAME
-tar -zcvf $BACKUPS_DIR/$BACKUP_NAME.tgz $BACKUP_NAME
-rm -rf $BACKUP_NAME
+BACKUP_NAME="/root/host/karr_lab/datanator/datanator/data_source/cache/$APP_NAME-$TIMESTAMP"
+ 
+$MONGODUMP_PATH -d $MONGO_DATABASE --host $MONGO_HOST -o $BACKUP_NAME
+
