9a435aa8dc7c210075b771f6bed5ef20ca0b8067
Zhouyang Lian <zhouyang.lian@familian.life>
2019-06-13 18:33:25 -0400

    calc_tanimoto.py

--- datanator/util/calc_tanimoto.py
+++ datanator/util/calc_tanimoto.py
@@ -12,10 +12,20 @@ class CalcTanimoto(mongo_util.MongoUtil):
     def __init__(self, cache_dirname=None, MongoDB=None, replicaSet=None, db=None,
                  verbose=False, max_entries=float('inf'), username=None,
                  password=None, authSource='admin'):
-    super(CalcTanimoto, self).__init__(cache_dirname=cache_dirname, MongoDB=MongoDB, replicaSet=replicaSet,
-                                    db=db, verbose=verbose, max_entries=max_entries, username=username,
-                                    password=password, authSource=authSource)
+        super(CalcTanimoto, self).__init__(cache_dirname=cache_dirname, MongoDB=MongoDB, replicaSet=replicaSet,
+                                           db=db, verbose=verbose, max_entries=max_entries, username=username,
+                                           password=password, authSource=authSource)
 
-    def get_tanimoto(self, mol1, mol2):
-        _, _, col_ecmdb = self.con_db('ecmdb')
-        _, _, col_ymdb = self.con_db('ymdb')
+    def get_tanimoto(self, mol1, mol2, str_format='inchi'):
+        '''Calculates tanimoto coefficients between
+                two molecules, mol1 and mol2
+                Args:
+                        mol1: molecule 1 in some format
+                        mol2: molecule 2 in same format as molecule 1
+                Return:
+                        tani: Tanimoto coefficient
+        '''
+        inchi = [mol1, mol2]
+        mols = [pybel.readstring(str_format, x) for x in inchi]
+        fps = [x.calcfp() for x in mols]
+        return round((fps[0] | fps[1]), 3)
--- tests/util/test_calc_tanimoto.py
+++ tests/util/test_calc_tanimoto.py
@@ -1,8 +1,9 @@
 import unittest
-from datanator.core import mongo_util
+from datanator.util import calc_tanimoto
 import tempfile
 import shutil
 from datanator.util import server_util
+
 
 class TestCalcTanimoto(unittest.TestCase):
 
@@ -11,14 +12,18 @@ class TestCalcTanimoto(unittest.TestCase):
         cls.cache_dirname = tempfile.mkdtemp()
         cls.db = 'datanator'
         config_file = '/root/host/karr_lab/datanator/.config/config.ini'
-        username, password, server, port = server_util.ServerUtil(config_file = config_file).get_user_config()
-        cls.src = mongo_util.MongoUtil(
+        username, password, server, port = server_util.ServerUtil(
+            config_file=config_file).get_user_config()
+        cls.src = calc_tanimoto.CalcTanimoto(
             cache_dirname=cls.cache_dirname, MongoDB=server, replicaSet=None, db=cls.db,
-                 verbose=True, max_entries=20, password = password, username = username)
+            verbose=True, max_entries=20, password=password, username=username)
 
     @classmethod
     def tearDownClass(cls):
         shutil.rmtree(cls.cache_dirname)
 
     def test_get_tanimoto(self):
-    	pass
+        mol1 = 'InChI=1S/C17H21N4O9P/c1-7-3-9-10(4-8(7)2)21(15-13(18-9)16(25)20-17(26)19-15)5-11(22)14(24)12(23)6-30-31(27,28)29'
+        mol2 = 'InChI=1S/C10H7NO3/c12-9(10(13)14)7-5-11-8-4-2-1-3-6(7)8/h1-5,11H,(H,13,14)'
+        coe = self.src.get_tanimoto(mol1, mol2)
+        self.assertEqual(0.121, coe)
