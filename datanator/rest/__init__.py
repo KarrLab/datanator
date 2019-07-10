0fc43cf59e84fe597cd60d38ac57d65d9f4640f6
Zhouyang Lian <zhouyang.lian@familian.life>
2019-07-10 16:55:51 -0400

    allow cors

--- datanator/rest/__init__.py
+++ datanator/rest/__init__.py
@@ -1,11 +1,13 @@
 import os
 
 from flask import Flask
+from flask_cors import CORS
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
