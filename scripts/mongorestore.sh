0e1d469abb9a1c997acf8d7bf0f5c28b24963603
Zhouyang Lian <zhouyang.lian@familian.life>
2019-07-15 10:26:23 -0400

    check for archive in mongorestore.sh

--- scripts/mongorestore.sh
+++ scripts/mongorestore.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-curl -o /root/host/karr_lab/datanator.archive https://mongo-dbdump.s3.amazonaws.com/datanator.20190701.archive
-mongorestore -d datanator --host mongo:27017 --archive=/root/host/karr_lab/datanator.archive
+FILE=/root/host/karr_lab/datanator.archive
+if [ ! -f "$FILE" ]; then
+	curl -o $FILE https://mongo-dbdump.s3.amazonaws.com/datanator.20190701.archive
+fi
+mongorestore -d datanator --host mongo:27017 --archive=$FILE
