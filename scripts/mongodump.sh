0892f3f3a21473f947c982f0f8162979e9fbf2dd
Zhouyang Lian <zhouyang.lian@familian.life>
2019-07-15 11:33:30 -0400

    ver 1.1 for mongodump cron script

--- scripts/mongodump.sh
+++ scripts/mongodump.sh
@@ -11,6 +11,6 @@ BACKUPS_DIR="/data/mongodump"
 mkdir -p $BACKUPS_DIR
 BACKUP_NAME="$BACKUPS_DIR/$APP_NAME"
  
-$MONGODUMP_PATH -d $MONGO_DATABASE -u $user -p $password --host $MONGO_HOST -o $BACKUP_NAME
+$MONGODUMP_PATH -d $MONGO_DATABASE -u $user -p $password --authenticationDatabase admin -o $BACKUP_NAME
 aws s3 cp $BACKUP_NAME s3://mongo-dbdump/ --recursive
 rm -rf $BACKUPS_DIR
