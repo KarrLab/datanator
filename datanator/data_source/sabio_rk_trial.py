8f04c4d5970af49c34bbc4cc4a9af68f7a4a1ee7
Zhouyang Lian <zhouyang.lian@familian.life>
2019-07-17 13:03:17 -0400

    removed references to suds3 in sabio_rk_trial

--- datanator/data_source/sabio_rk_trial.py
+++ datanator/data_source/sabio_rk_trial.py
@@ -1,12 +1,17 @@
-from suds.client import Client
-from suds.transport import TransportError
+import datanator.config.core
+from datanator.util import mongo_util
+
 
 class SabioRk:
 
-    def suds_trial(self):
-        url = "http://sabio.villa-bosch.de/sabiork?wsdl"
-        try:
-            client = Client(url)
-            print(client.service.getPathwayNames("ABC"))
-        except TransportError as e:
-            return str(e)
+    def __init__(self, cache_dirname=None, MongoDB=None, replicaSet=None, db=None,
+                 verbose=False, max_entries=float('inf'), username=None,
+                 password=None, authSource='admin'):
+        self.cache_dirname = cache_dirname
+        self.MongoDB = MongoDB
+        self.replicaSet = replicaSet
+        self.db = db
+        self.verbose = verbose
+        self.max_entries = max_entries
+        self.client, self.db_obj, self.collection = mongo_util.MongoUtil(
+            MongoDB=MongoDB, db=db, username=username, password=password, authSource=authSource)
--- tests/data_source/test_sabio_rk_trial.py
+++ tests/data_source/test_sabio_rk_trial.py
@@ -8,11 +8,23 @@ class TestSabioRk(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.cache_dirname = tempfile.mkdtemp()
-        cls.src = sabio_rk_trial.SabioRk()
+        db = 'test'
+        username = datanator.config.core.get_config()[
+            'datanator']['mongodb']['user']
+        password = datanator.config.core.get_config(
+        )['datanator']['mongodb']['password']
+        MongoDB = datanator.config.core.get_config(
+        )['datanator']['mongodb']['server']
+        port = datanator.config.core.get_config(
+        )['datanator']['mongodb']['port']
+        replSet = datanator.config.core.get_config(
+        )['datanator']['mongodb']['replSet']
+        cls.src = sabio_rk_trial.SabioRk(cache_dirname=cls.cache_dirname,
+                                         MongoDB=MongoDB,  db=db,
+                                         verbose=True, max_entries=20, username=username,
+                                         password=password)
 
     @classmethod
     def tearDownClass(cls):
         shutil.rmtree(cls.cache_dirname)
-
-    def test_suds_trial(self):
-        self.assertEqual(self.src.suds_trial(), 'HTTP Error 404: Not Found')
+        cls.src.client.close()
