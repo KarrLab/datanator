976755f2b6569016db302e5b6e61cbbb410e6bab
Zhouyang Lian <zhouyang.lian@familian.life>
2019-07-18 11:37:18 -0400

    use a specific version of karrlab/datanator from quilt

--- scripts/export_quilt.py
+++ scripts/export_quilt.py
@@ -1,4 +1,4 @@
 import quilt
 
-quilt.install("karrlab/datanator")
+quilt.install("karrlab/datanator", hash='d8754b82a708b22f4389bd8fe6dc40a38b9d9ee5364a942c7cef78bc28fe163b')
 quilt.export("karrlab/datanator", "./datanator/data_source/cache")
