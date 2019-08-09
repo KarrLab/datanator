83342bac887941c4b6732e837b827744a8a5c289
Zhouyang Lian <zhouyang.lian@familian.life>
2019-08-09 16:15:17 -0400

    protein aggregate collection

--- datanator/data_source/protein_aggregate.py
+++ datanator/data_source/protein_aggregate.py
@@ -12,8 +12,25 @@ class ProteinAggregate:
     	self.verbose = verbose	
     	self.mongo_manager = mongo_util.MongoUtil(MongoDB=server, username=username,
     		password=password, authSource=authSource, db=database)
+    	self.pax_manager = query_pax.QueryPax(MongoDB=server, db=database,
+                 collection_str='pax', verbose=verbose, max_entries=max_entries, username=username,
+                 password=password, authSource=authSource)
 
-    def load_content(self):
+    def load_content(self, aggregate='protein'):
     	_, _, col_uniprot = self.mongo_manager.con_db('uniprot')
+    	_, _, col_protein = self.mongo_manager.con_db(aggregate)
     	query = {}
     	projection = {'status': 0, '_id': 0}
+    	docs = col_uniprot.find_one(filter=query, projection=projection)
+    	count = col_uniprot.count_documents({})
+    	for i, doc in enumerate(docs):
+    		if i == max_entries:
+    			break
+    		if self.verbose and i % 10 == 0:
+    			print('Processing doc {} of {} ...'.format(i, count))
+    		uniprot_id = doc['uniprot_id']
+    		abundances = self.pax_manager.get_abundance_from_uniprot(uniprot_id)
+    		doc['abundances'] = abundances
+    		col_protein.update_one({'uniprot_id': doc['uniprot_id']},
+    			doc,
+    			upsert=True)
