4f1405dfc4143fcf1d275ed5005e1bce67570a12
zhouyang.lian <zhouyang.lian@familian.life>
2019-12-03 15:20:20 -0500

    rna_halflife_util; new doi to parse

---
+++ datanator/data_source/rna_halflife/doi_10_1093_nar_gks1019.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from datanator.util import mongo_util, rna_halflife_util
+import json
+import datetime
+import datanator.config.core
+
+
+class Halflife(rna_halflife_util.RnaHLUtil):
+
+    def __init__(self, cache_dir=None, server=None, src_db=None, protein_col=None,
+                authDB=None, readPreference=None, username=None, password=None,
+                verbose=None, max_entries=None, des_db=None, rna_col=None):
+        """Init
+        
+        Args:
+            cache_dir (:obj:`str`, optional): Cache directory for logs. Defaults to None.
+            server (:obj:`str`, optional): MongoDB server address. Defaults to None.
+            db (:obj:`str`, optional): Database where initial uniprot collection resides. Defaults to None.
+            collection_str (:obj:`str`, optional): name of collection. Defaults to None.
+            authDB (:obj:`str`, optional): MongoDB authentication database. Defaults to None.
+            readPreference (:obj:`str`, optional): MongoDB read preference. Defaults to None.
+            username (:obj:`str`, optional): MongoDB username. Defaults to None.
+            password (:obj:`str`, optional): MongoDB password. Defaults to None.
+            verbose (:obj:`bool`, optional): Wheter to display verbose messages. Defaults to None.
+            max_entries (:obj:`int`, optional): Number of records to be processed. Defaults to None.
+            uniprot_col_db (:obj:`int`, optional): Database to which new uniprot records will be inserted. Defaults to None.
+        """
+        super(Halflife, self).__init__(self, server=server, username=username, password=password, src_db=src_db,
+        des_db=des_db, protein_col=protein_col, rna_col=rna_col, authDB=authDB, readPreference=readPreference,
+        max_entries=max_entries, verbose=verbose)
+        self.max_entries = max_entries
+
+    def fill_uniprot(self):
+        pass
