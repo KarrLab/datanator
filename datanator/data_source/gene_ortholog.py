7ec23bb31aa7aa8758a70f3c94f1789531539e28
zhouyang.lian <zhouyang.lian@familian.life>
2020-02-05 16:15:33 -0500

    refactored kegg_orthology parsing to include disease, pathway and module information

--- datanator/data_source/gene_ortholog.py
+++ datanator/data_source/gene_ortholog.py
@@ -189,7 +189,7 @@ def main():
     )['datanator']['mongodb']['server']
     manager = KeggGeneOrtholog(server, collection_str=collection_str, username=username,
     password=password, des_db=des_db)
-    manager.load_data(skip=370)
+    manager.load_data(skip=770)
 
 if __name__ == '__main__':
     main()
