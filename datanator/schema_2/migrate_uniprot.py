8b2d44ab1d6ae4312560ed7fab99bd00a9f871a0
Zhouyang <zhouyang.lian@familian.life>
2020-06-12 13:29:41 -0400

    migrate taxon_tree

---
+++ datanator/schema_2/migrate_uniprot.py
@@ -0,0 +1,59 @@
+from datanator_query_python.config import query_schema_2_manager
+from datanator_query_python.config import config
+from datanator_query_python.query import query_taxon_tree
+
+
+class MigrateUniprot(query_schema_2_manager.QM):
+
+    def __init__(self, readPreference='primary', collection='uniprot',
+                max_entries=float('inf'), username=config.SchemaMigration.USERNAME,
+                password=config.SchemaMigration.PASSWORD):
+        super().__init__(readPreference=readPreference, username=username,
+                        password=password)
+        self.collection = collection
+        self.from_collection = self.client.get_database('datanator')[collection]
+        self.to_collection = self.client.get_database('datanator-test')[collection]
+        self.max_entries = max_entries
+
+    def index_primary(self, _key):
+        """Index key (single key ascending)
+
+        Args:
+            _key(:obj:`str`): Name of key to be indexed
+        """
+        self.to_collection.create_index(_key)
+    
+    def get_multi_cursor(self, count):
+        """Manually return multiple cursors.
+
+        Args:
+            count(:obj:`int`): Number of cursors wanted.
+
+        Return:
+            (:obj:`list` of :obj:`pymongo.Cursor`)
+        """
+        query = {}
+        total_docs = self.from_collection.count_documents(query)
+        step_size = int((total_docs % count + total_docs) / count)
+        result = []
+        for i in range(0, count):
+            result.append(self.from_collection.find(filter=query, skip=i * step_size, limit=step_size))
+        return result
+    
+    def process_cursor(self, docs):
+        """Process mongodb cursor (for parallel processing)
+        Transform data and move to new database
+
+        Args:
+            docs(:obj:`pymongo.Cursor`): documents to be processed
+        """
+        for i, doc in enumerate(docs):
+            if i == self.max_entries:
+                break
+            doc["add_id"] = [{"name_space": "gene_name_alt", "value": doc.get("gene_name_alt")},
+                             {"name_space": "gene_name_orf", "value": doc.get("gene_name_orf")},
+                             {"name_space": "gene_name_oln", "value": doc.get("gene_name_oln")}]
+            doc.pop('gene_name_alt', None)
+            doc.pop('gene_name_orf', None)
+            doc.pop('gene_name_oln', None)
+        pass
