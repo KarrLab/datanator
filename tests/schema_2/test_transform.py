41a627e04884fc229b2a9f80ac1c3c3f99fed2bf
Zhouyang <zhouyang.lian@familian.life>
2020-07-07 17:54:23 -0400

    added test_transform.py

---
+++ tests/schema_2/test_transform.py
@@ -0,0 +1,14 @@
+from datanator.schema_2 import transform
+from datanator_query_python.config import config
+import unittest
+
+
+class TestTransform(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
