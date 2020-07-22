7e1d501eedd71cf09c54a9faf6c432cb2c47a37d
Jonathan Karr <jonrkarr@gmail.com>
2020-07-22 16:39:16 -0400

    fixing invalid line endings

--- datanator/data_source/protein_localization/experimental.py
+++ datanator/data_source/protein_localization/experimental.py
@@ -8,21 +8,21 @@ from datanator_query_python.util import mongo_util
 class ParsePsortExperimental(mongo_util.MongoUtil):
 
     def __init__(self, max_entries=float('inf'),
-                       MongoDB=None,
-                       db=None,
-                       collection=None,
-                       username=None,
-                       password=None,
-                       authSource='admin',
-                       readPreference='nearest'):
-        super().__init__(MongoDB=MongoDB, db=db,
-                        username=username,
-                        password=password,
-                        authSource=authSource,
-                        readPreference=readPreference)
+                 MongoDB=None,
+                 db=None,
+                 collection=None,
+                 username=None,
+                 password=None,
+                 authSource='admin',
+                 readPreference='nearest'):
+        super(ParsePsortExperimental, self).__init__(MongoDB=MongoDB, db=db,
+                                                     username=username,
+                                                     password=password,
+                                                     authSource=authSource,
+                                                     readPreference=readPreference)
         self.max_entries = max_entries
         self.collection = collection
-​
+
     def parse_psortdb(self):
         """
         To parse database psortdb Experimental-PSORTdb-v4.00.tsv file
@@ -36,31 +36,30 @@ class ParsePsortExperimental(mongo_util.MongoUtil):
             ()
         """
         collection = self.db_obj[self.collection]
-        # data=pd.read_csv('Experimental-PSORTdb-v4.00.tsv',delimiter="\t")
+        # data = pd.read_csv('Experimental-PSORTdb-v4.00.tsv', delimiter="\t")
         # data = data.fillna("None")
         # header = list(data.columns.values)
         # for i in range(self.max_entries):
-        #     d={}
+        #     d = {}
         #     for j in range(len(header)):
-        #         if isinstance(data.iloc[i,j],int):
-        #             data.iloc[i,j]=int(data.iloc[i,j])
+        #         if isinstance(data.iloc[i, j], int):
+        #             data.iloc[i, j] = int(data.iloc[i, j])
         #         else:
-        #             d[header[j]] = data.iloc[i,j]
-        #     #name is the JSON file's name
-        #     if (data.iloc[i,0]!="None"):
-        #         name = data.iloc[i,0]   #SwissProt_ID
+        #             d[header[j]] = data.iloc[i, j]
+        #     # name is the JSON file's name
+        #     if (data.iloc[i, 0] != "None"):
+        #         name = data.iloc[i, 0]  # SwissProt_ID
         #     else:
-        #         name = data.iloc[i,2]   #Other_Accession
-        #     with open(name+".json","w+") as f:
-        #         x = json.dumps(d,f,cls=NpEncoder)
+        #         name = data.iloc[i, 2]  # Other_Accession
+        #     with open(name + ".json", "w+") as f:
+        #         x = json.dumps(d, f, cls=NpEncoder)
         collection.update_one({"uniprot_id": "P01234"},
-                                {"$set": {"protein_name": "some_name",
+                              {"$set": {"protein_name": "some_name",
                                         "another_field": "another_value"},
-                                "$addToSet": {"$each": {"add_id": [{"namespace": "something",
-                                                            "value": "1"}]}}})
-            
-            
-​
+                               "$addToSet": {"$each": {"add_id": [{"namespace": "something",
+                                                                   "value": "1"}]}}})
+
+
 def main():
     conf = config.Justin()
     conf_main = config.Config()
@@ -72,7 +71,7 @@ def main():
                                  password=password,
                                  collection="protein_localization",
                                  db="datanator-demo")
-    src.parse_psortdb()    
+    src.parse_psortdb()
 
 
 if __name__ == '__main__':
